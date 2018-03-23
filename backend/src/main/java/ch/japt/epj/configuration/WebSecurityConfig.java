103902459306519e208c69774a0f27a8c3334e44
SBI- <pascal.huerlimann@gmail.com>
2018-03-23 16:13:27 +0100

    Allow access to all areas without login

--- backend/src/main/java/ch/japt/epj/configuration/WebSecurityConfig.java
+++ backend/src/main/java/ch/japt/epj/configuration/WebSecurityConfig.java
@@ -22,19 +22,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/static/**/*"
         };
 
-        http
-                .authorizeRequests()
-                    .antMatchers(allowed).permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    // uncommenting this should enable a default login form
-                    // we need to replace this with a custom one
-                    //.loginPage("login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+//        http
+//                .authorizeRequests()
+//                    .antMatchers(allowed).permitAll()
+//                    .anyRequest().authenticated()
+//                    .and()
+//                .formLogin()
+//                    // uncommenting this should enable a default login form
+//                    // we need to replace this with a custom one
+//                    //.loginPage("login")
+//                    .permitAll()
+//                    .and()
+//                .logout()
+//                    .permitAll();
 
         // This is key for exposing csrf tokens in apis that are outside
         // of the browser. We will need these headers in react and for
