8b7a04a5445fd5bf58228e8e6bcf164f75c68a42
SBI- <pascal.huerlimann@gmail.com>
2018-03-29 12:49:24 +0200

    Add random payload

--- backend/src/main/java/ch/japt/epj/api/Test.java
+++ backend/src/main/java/ch/japt/epj/api/Test.java
@@ -2,12 +2,13 @@ package ch.japt.epj.api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
     @RequestMapping(path = "/api/test", method = RequestMethod.GET)
-    public String test() {
-        return "working";
+    public @ResponseBody TestPayload test() {
+        return new TestPayload();
     }
 }
--- backend/src/main/java/ch/japt/epj/api/TestPayload.java
+++ backend/src/main/java/ch/japt/epj/api/TestPayload.java
@@ -1,4 +1,9 @@
 package ch.japt.epj.api;
 
 public class TestPayload {
+    public final double random;
+
+    public TestPayload() {
+        this.random = Math.random();
+    }
 }
