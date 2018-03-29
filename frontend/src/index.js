f9f6dbe53607d2f0bfa1e6de2533bb1200cc27ec
SBI- <pascal.huerlimann@gmail.com>
2018-03-29 11:46:43 +0200

    Create react config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log(process.env);
-
-if (process.env.NODE_ENV === 'development') {
-    // set docker as server
-}
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
