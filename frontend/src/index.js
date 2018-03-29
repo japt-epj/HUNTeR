d030a09418bcfd65bfabe82515110990e8088a07
SBI- <pascal.huerlimann@gmail.com>
2018-03-29 09:50:08 +0200

    Log environment configuration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log(process.env);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
