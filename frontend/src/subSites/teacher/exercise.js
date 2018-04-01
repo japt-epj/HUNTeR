6db23428b8f7f76dd521750f23a6cb91702f3c1c
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-04-01 19:27:38 +0200

    Create of frontend form for creating an exercise

--- frontend/src/subSites/teacher/exercise.js
+++ frontend/src/subSites/teacher/exercise.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Form, Button, Table} from 'semantic-ui-react';
+import axios from 'axios';
 
 import TableHandler from '../../handlers/TableHandler';
 
@@ -7,44 +8,91 @@ import TableHandler from '../../handlers/TableHandler';
 class Exercise extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            title: '',
+            question: '',
+            answerOptions: {
+                A: {text: '', correct: false},
+                B: {text: '', correct: false},
+                C: {text: '', correct: false},
+                D: {text: '', correct: false}
+            },
+            explanation: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const regex = /(option(Answer|Checkbox))([A-D])/;
+        const match = regex.exec(name);
+        if (match === null) {
+            this.setState({[name]: target.value});
+        } else if (match[1] === 'optionAnswer') {
+            let answerOptions = {...this.state.answerOptions};
+            answerOptions[match[3]].text = target.value;
+            this.setState({answerOptions});
+        } else if (match[1] === 'optionCheckbox') {
+            let answerOptions = {...this.state.answerOptions};
+            answerOptions[match[3]].correct = target.checked;
+            this.setState({answerOptions});
+        }
+        console.log(this.state);
+    }
+
+    handleSubmit() {
+        axios.post('https://jsonplaceholder.typicode.com/posts',
+            this.state
+        ).then(res => console.log(res))
+            .catch(e => console.log(e));
     }
 
     render() {
         return (
-            <div className="content">
-                <Form>
-                    <Form.Input fluid label="Titel" placeholder="Bitte geben Sie einen Titel ein"/>
-                    <Form.TextArea label="Aufgabenfrage" placeholder="Bitte geben Sie eine Frage ein..."/>
-                    <Table definition>
-                        <Table.Header>
-                            <Table.Row>
-                                {TableHandler.getTableHeader(['', 'Antworten', 'Richtig'])}
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {new Array(4).fill().map((item, index) => {
-                                const ANSWER = String.fromCharCode(65 + index);
-                                return (
-                                    <Table.Row key={'TableRow' + index}>
-                                        {TableHandler.getTableCell({element: ANSWER, collapsed: true})}
-                                        {TableHandler.getTableCell({
-                                            element: (<Form.Input fluid placeholder="Bitte Antwort eingeben"/>),
-                                            collapsed: false
-                                        })}
-                                        {TableHandler.getTableCell({
-                                            element: (<Form.Field control="input" type="checkbox"/>),
-                                            collapsed: true
-                                        })}
-                                    </Table.Row>
-                                );
-                            })}
-                        </Table.Body>
-                    </Table>
-                    <Form.TextArea label="Erklärungstext"
-                                   placeholder="Bitte geben Sie eine Erklärungstext ein..."/>
-                    <Button> Submit </Button>
-                </Form>
-            </div>
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Input fluid label="Titel" name="title" value={this.title} onChange={this.handleChange}
+                            placeholder="Bitte geben Sie einen Titel ein"/>
+                <Form.TextArea label="Aufgabenfrage" name="question" value={this.question} onChange={this.handleChange}
+                               placeholder="Bitte geben Sie eine Frage ein..."/>
+                <Table definition>
+                    <Table.Header>
+                        <Table.Row>
+                            {TableHandler.getTableHeader(['', 'Antworten', 'Richtig'])}
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {new Array(4).fill().map((item, index) => {
+                            const ANSWER = String.fromCharCode(65 + index);
+                            return (
+                                <Table.Row key={'TableRow' + index}>
+                                    {TableHandler.getTableCell({element: ANSWER, collapsed: true})}
+                                    {TableHandler.getTableCell({
+                                        element: (<Form.Input fluid name={'optionAnswer' + ANSWER}
+                                                              value={this.state.answerOptions[ANSWER].text}
+                                                              onChange={this.handleChange}
+                                                              placeholder="Bitte Antwort eingeben"/>),
+                                        collapsed: false
+                                    })}
+                                    {TableHandler.getTableCell({
+                                        element: (
+                                            <Form.Field control="input" type="checkbox"
+                                                        name={'optionCheckbox' + ANSWER}
+                                                        onChange={this.handleChange}
+                                                        checked={this.state.answerOptions[ANSWER].correct}/>),
+                                        collapsed: true
+                                    })}
+                                </Table.Row>
+                            );
+                        })}
+                    </Table.Body>
+                </Table>
+                <Form.TextArea label="Erklärungstext" name="explanation" value={this.question}
+                               onChange={this.handleChange}
+                               placeholder="Bitte geben Sie eine Erklärungstext ein..."/>
+                <Button> Submit </Button>
+            </Form>
         );
     }
 }
