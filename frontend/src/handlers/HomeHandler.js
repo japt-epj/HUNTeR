46c245f1860ec9647f525e5c92d29d68253585db
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-04-06 01:20:02 +0200

    Merge Home Site of student and teacher to HomeHandler

--- frontend/src/handlers/HomeHandler.js
+++ frontend/src/handlers/HomeHandler.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import {NavLink, Route} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 import {Menu} from 'semantic-ui-react';
 
-import Data from '../../data/Data';
 
-
-export default function getHome() {
+export default function getHome(paths) {
     return (
         <div>
             <Menu fluid vertical>
-                {Data.getPathsTeacher().map((element) =>
+                {paths.map((element) =>
                     <Menu.Item key={'menuItem' + element.path}><NavLink key={'NavLink' + element.path}
                                                                         to={'/' + element.path}>{element.title}</NavLink></Menu.Item>
                 )}
             </Menu>
             <div className="content">
-                {Data.getPathsTeacher().map((element) =>
-                    <Route key={'Route' + element.path} path={'/' + element.path} component={element.component}/>
+                {paths.map((element) =>
+                    <Route key={'Route' + element.path} path={'/' + element.path}
+                           component={element.component}/>
                 )}
             </div>
         </div>
