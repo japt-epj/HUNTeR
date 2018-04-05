5f751e457e19594f976a03144451e52a3c1803a2
SBI- <pascal.huerlimann@gmail.com>
2018-04-05 11:03:59 +0200

    Delete unused examples

--- backend/src/main/java/ch/japt/epj/HunterRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.japt.epj;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface HunterRepository extends CrudRepository<Hunter, Long> {
-    Optional<Hunter> findByName(String name);
-}
