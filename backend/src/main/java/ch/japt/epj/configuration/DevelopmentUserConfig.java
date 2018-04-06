6a92bd51ba742072f61ea400f9cd33671b52e305
SBI- <pascal.huerlimann@gmail.com>
2018-04-06 15:19:13 +0200

    Disable security and csrf

--- backend/src/main/java/ch/japt/epj/configuration/DevelopmentUserConfig.java
+++ backend/src/main/java/ch/japt/epj/configuration/DevelopmentUserConfig.java
@@ -1,19 +1,16 @@
 package ch.japt.epj.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class DevelopmentUserConfig {
-    @Autowired
-    @Profile("dev")
-    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        builder.inMemoryAuthentication()
-                .withUser("test")
-                .password("test")
-                .roles("USER");
-    }
+//    @Autowired
+//    @Profile("dev")
+//    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+//        builder.inMemoryAuthentication()
+//                .withUser("test")
+//                .password("test")
+//                .roles("USER");
+//    }
 
 }
