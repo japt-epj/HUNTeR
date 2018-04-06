299b5ad55d63283a4d2cbe13ac0cf1c6d5c5ea0c
SBI- <pascal.huerlimann@gmail.com>
2018-04-06 14:43:11 +0200

    Add post support for exercise endpoint

--- backend/src/main/java/ch/japt/epj/api/ExerciseController.java
+++ backend/src/main/java/ch/japt/epj/api/ExerciseController.java
@@ -3,15 +3,17 @@ package ch.japt.epj.api;
 import ch.japt.epj.model.Answer;
 import ch.japt.epj.model.Task;
 import ch.japt.epj.model.dto.ExerciseDto;
+import ch.japt.epj.model.dto.NewExerciseDto;
+import ch.japt.epj.repository.AnswerRepository;
 import ch.japt.epj.repository.ExerciseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,6 +22,9 @@ import java.util.List;
 public class ExerciseController implements ExerciseApi {
     @Autowired
     private ExerciseRepository repository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
 
     private ExerciseDto map(Task task) {
         ExerciseDto dto = new ExerciseDto();
@@ -31,8 +36,21 @@ public class ExerciseController implements ExerciseApi {
     }
 
     @Override
-    public ResponseEntity<Void> addExercise(ExerciseDto body) {
-        return null;
+    public ResponseEntity<Void> addExercise(@RequestBody NewExerciseDto body) {
+        Task task = new Task();
+        task.setName(body.getTitle());
+        task.setQuestion(body.getQuestion());
+
+        body.getAnswers().forEach(newAnswer -> {
+            Answer answer = new Answer();
+            answer.setAnswer(newAnswer.getText());
+            answer.setChecked(newAnswer.isChecked());
+            task.addAnswer(answer);
+            answerRepository.save(answer);
+        });
+
+        repository.save(task);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @Override
---
+++ backend/src/main/java/ch/japt/epj/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package ch.japt.epj.repository;
+
+import ch.japt.epj.model.Answer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Long> {
+}
