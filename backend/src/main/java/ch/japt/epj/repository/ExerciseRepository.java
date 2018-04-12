7476cbf6c95287af53b2ba809d3e01bb77c7fbda
SBI- <pascal.huerlimann@gmail.com>
2018-04-12 17:22:53 +0200

    Move model data classes to package

--- backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
+++ backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
@@ -1,6 +1,6 @@
 package ch.japt.epj.repository;
 
-import ch.japt.epj.model.Task;
+import ch.japt.epj.model.data.Task;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExerciseRepository extends CrudRepository<Task, Long> {
