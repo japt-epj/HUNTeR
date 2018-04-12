df3eb0cd88af6838822721b8b2b53d0527585a68
SBI- <pascal.huerlimann@gmail.com>
2018-04-12 14:54:47 +0200

    Remove frontend from dev build

--- dev.sh
+++ dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SPRING_PROFILES_ACTIVE=dev; ./mvnw "-DskipTests=true" "-Ddeactivate-dev-tools=false" clean install
+# check if target/bla.jar exists in frontend
+SPRING_PROFILES_ACTIVE=dev; ./mvnw -f backend/pom.xml "-DskipTests=true" "-Ddeactivate-dev-tools=false" clean install
 
 # TODO: check for build success here.
 
@@ -12,12 +13,12 @@ else
     docker-compose -f docker/docker-compose.yaml build && docker-compose -f docker/docker-compose.yaml up -d
 fi
 
+# only start frontend if build and up worked
 if [ $? -ne 0 ]; then
     echo "Docker Build failed, frontend not started"
     exit 1
 fi
 
-# TODO: only start frontend if build and up worked
 cd frontend
 npm install
 npm run start
