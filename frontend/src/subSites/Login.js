bebeeef13bf391a86374eac22152292f48c4447a
AndiHSR <andi.hoerler@hsr.ch>
2018-04-12 17:31:08 +0200

    Create login page

---
+++ frontend/src/subSites/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {BrowserRouter, NavLink} from 'react-router-dom';
+
+import {Form, Grid} from 'semantic-ui-react';
+
+import StructureHandler from '../handlers/StructureHandler';
+import FormHandler from '../handlers/FormHandler';
+import APIHandler from '../handlers/APIHandler';
+
+
+export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.getLoginHeader = StructureHandler.getLoginHeader.bind(this);
+        this.handleSubmit = FormHandler.handleLoginSubmit.bind(this);
+        this.handleChange = FormHandler.handleChange.bind(this);
+        this.postData = APIHandler.postData.bind(this);
+    }
+
+    render() {
+        return (
+            <BrowserRouter basename="/">
+                <Grid className={"siteGrid"} padded>
+                    {this.getLoginHeader()}
+                    <Grid.Row className="gridContent" centered>
+                        <div>
+                            <Form onSubmit={this.handleSubmit}>
+                                <Form.Input label="E-Mail-Adresse" type="email" name="email"
+                                            value={this.state.email}
+                                            onChange={this.handleChange} required/>
+                                <Form.Input label="Passwort" type="password" name="password" value={this.state.password}
+                                            onChange={this.handleChange} required/>
+                                <Form.Button content='Submit'/>
+                            </Form>
+                            <p><NavLink to="/student">Student</NavLink></p>
+                            <p><NavLink to="/teacher">Teacher</NavLink></p>
+                        </div>
+                    </Grid.Row>
+                </Grid>
+            </BrowserRouter>
+        )
+    }
+}
