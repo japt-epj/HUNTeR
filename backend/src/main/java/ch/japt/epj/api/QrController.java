bdf8072c116ca1b124fc33bfcb3a3660df666645
SBI- <pascal.huerlimann@gmail.com>
2018-04-13 18:21:31 +0200

    Create qr model and implement functional style controller

--- backend/src/main/java/ch/japt/epj/api/QrController.java
+++ backend/src/main/java/ch/japt/epj/api/QrController.java
@@ -1,31 +1,29 @@
 package ch.japt.epj.api;
 
-import io.nayuki.qrcodegen.QrCode;
+import ch.japt.epj.model.QrModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/api")
 public class QrController implements QrCodeApi {
+    private final QrModel model;
+
+    public QrController(@Autowired QrModel model) {
+        this.model = model;
+    }
+
     @Override
     public ResponseEntity<byte[]> getQRCode(@PathVariable("id") Integer id) {
-        QrCode code = QrCode.encodeText(String.valueOf(id), QrCode.Ecc.MEDIUM);
-        BufferedImage image = code.toImage(20, 2);
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-
-        try {
-            ImageIO.write(image, "png", stream);
-            return new ResponseEntity<>(stream.toByteArray(), HttpStatus.OK);
-        } catch (IOException e) {
-            return new ResponseEntity<>(new byte[0], HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return model.generateCode(id)
+                .map(b -> new ResponseEntity(b, HttpStatus.OK))
+                .orElse(new ResponseEntity(new byte[0], HttpStatus.NOT_FOUND));
     }
 }
