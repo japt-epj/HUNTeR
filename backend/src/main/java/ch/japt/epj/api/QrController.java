c884ede7e2aae9d48961b0376cdc001007e05295
SBI- <pascal.huerlimann@gmail.com>
2018-04-13 15:24:55 +0200

    Implement simple qr code generation

--- backend/src/main/java/ch/japt/epj/api/QrController.java
+++ backend/src/main/java/ch/japt/epj/api/QrController.java
@@ -3,18 +3,22 @@ package ch.japt.epj.api;
 import io.nayuki.qrcodegen.QrCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+@Controller
+@RequestMapping("/api")
 public class QrController implements QrCodeApi {
     @Override
     public ResponseEntity<byte[]> getQRCode(@PathVariable("id") Integer id) {
         QrCode code = QrCode.encodeText(String.valueOf(id), QrCode.Ecc.MEDIUM);
-        BufferedImage image = code.toImage(5, 10);
+        BufferedImage image = code.toImage(20, 2);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
         try {
