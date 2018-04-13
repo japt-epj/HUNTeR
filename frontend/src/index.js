68e306d4cc2af9c45241edcd1ef1120154e8e60b
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-04-13 14:45:07 +0200

    Rename of Login.js to App.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './style/index.css';
 
 import registerServiceWorker from './registerServiceWorker';
-import Login from './subSites/Login';
+import App from './App';
 import Teacher from './subSites/teacher/TeacherStructure';
 import Student from './subSites/student/StudentStructure';
 
@@ -14,7 +14,7 @@ import Student from './subSites/student/StudentStructure';
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route exact path="/" component={Login}/>
+            <Route exact path="/" component={App}/>
             <Route path="/teacher" component={Teacher}/>
             <Route path="/student" component={Student}/>
         </Switch>
