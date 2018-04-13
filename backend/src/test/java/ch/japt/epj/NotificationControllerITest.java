062b89e4f1b858138fa9e64e7fcb4eeb924ef0cc
SBI- <pascal.huerlimann@gmail.com>
2018-04-13 09:34:17 +0200

    Remove unnceccessary imports

--- backend/src/test/java/ch/japt/epj/NotificationControllerITest.java
+++ backend/src/test/java/ch/japt/epj/NotificationControllerITest.java
@@ -1,8 +1,5 @@
 package ch.japt.epj;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-
 import java.net.URL;
 
 import org.junit.Before;
@@ -12,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
