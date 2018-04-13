39de0f3019c817e326d67ed8b901972fe24f1caa
TobiHSR <tobias.saladin@hsr.ch>
2018-04-13 11:24:23 +0200

    merge

--- frontend/src/subSites/teacher/TeacherQuiz.js
+++ frontend/src/subSites/teacher/TeacherQuiz.js
@@ -24,42 +24,99 @@ export default class extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.loading && this.state.loadingScreen}
-                <Form>
-                    <Grid>
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Form.Input fluid label="Titel"
-                                            placeholder="Bitte geben Sie einen Titel für das Quiz ein"/>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Table definition>
-                                    <Table.Header>
-                                        <Table.Row>
-                                            <Table.HeaderCell/>
-                                            <Table.HeaderCell colSpan="3">
-                                                Aufgaben welche im Quiz vorhanden sein sollten:
-                                            </Table.HeaderCell>
-                                        </Table.Row>
-                                    </Table.Header>
-                                    <Table.Body>
-                                        {!this.state.loading && this.state.table}
-                                        {this.getTableRows()}
-                                    </Table.Body>
-                                </Table>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Form.Button>Submit</Form.Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Form>
-            </div>
+            <Form>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Form.Input fluid label="Titel" placeholder="Bitte geben Sie einen Titel ein"/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Modal size="fullscreen"
+                                   trigger={<Button>Benutzer hinzufügen</Button>}
+                                   closeIcon>
+                                <Modal.Content>
+                                    <Modal.Description>
+                                        <Table>
+                                            <Table.Header>
+                                                <Table.Row>
+                                                    <Table.HeaderCell colSpan="4">Aufgaben im Quiz:</Table.HeaderCell>
+                                                </Table.Row>
+                                            </Table.Header>
+                                            <Table.Body>
+                                                {Data.getStudents().map((element, index) =>
+                                                    <Table.Row key={'TableRow' + index}>
+                                                        <grid>
+                                                            {TableHandler.getTableCell({
+                                                                element: (<Form.Field control="input"
+                                                                                      type="checkbox"/>),
+                                                                collapsed: true
+                                                            })}
+                                                            {TableHandler.getTableCell({
+                                                                element: (<Segment>{element.email}</Segment>),
+                                                                collapsed: false
+                                                            })}
+                                                        </grid>
+                                                    </Table.Row>
+                                                )}
+                                            </Table.Body>
+                                        </Table>
+                                    </Modal.Description>
+                                </Modal.Content>
+                                <Modal.Actions>
+                                    <Button color="green" icon="add" label="Hinzufügen"/>
+                                </Modal.Actions>
+                            </Modal>
+                            <Modal size="fullscreen"
+                                   trigger={<Button>Aufgaben hinzufügen</Button>}
+                                   closeIcon>
+                                <Modal.Header>{'Aufgaben hinzufügen'}</Modal.Header>
+                                <Modal.Content>
+                                    <Modal.Description>
+                                        <Table>
+                                            <Table.Header>
+                                                <Table.Row>
+                                                    <Table.HeaderCell colSpan="4">Aufgaben im Quiz:</Table.HeaderCell>
+                                                </Table.Row>
+                                            </Table.Header>
+                                            <Table.Body>
+                                                {Data.getExercises().map((element, index) =>
+                                                    <Table.Row key={'TableRow' + index}>
+                                                        {TableHandler.getTableCell({
+                                                            element: (<Form.Field control="input"
+                                                                                  type="checkbox"/>), collapsed: true
+                                                        })}
+                                                        {TableHandler.getTableCell({
+                                                            element: (<Segment>{element.title}</Segment>),
+                                                            collapsed: false
+                                                        })}
+                                                        {TableHandler.getTableCell({
+                                                            element: (
+                                                                <NavLink to={'/exercise?id=' + element.key}>
+                                                                    <Button basic icon="edit"
+                                                                            color="green"/></NavLink>),
+                                                            collapsed: true
+                                                        })}
+                                                    </Table.Row>
+                                                )}
+                                            </Table.Body>
+                                        </Table>
+                                    </Modal.Description>
+                                </Modal.Content>
+                                <Modal.Actions>
+                                    <Button color="green" icon="add" label="Hinzufügen"/>
+                                </Modal.Actions>
+                            </Modal>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Form.Button>Submit</Form.Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Form>
         );
     }
 }
