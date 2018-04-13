ba8902ae47a3fe6b82b6fa82e6936b3fec572b1b
AndiHSR <andi.hoerler@hsr.ch>
2018-04-13 13:30:42 +0200

    Redo change to Login.js

--- frontend/src/subSites/Login.js
+++ frontend/src/subSites/Login.js
@@ -8,7 +8,7 @@ import FormHandler from '../handlers/FormHandler';
 import APIHandler from '../handlers/APIHandler';
 
 
-class Login extends React.Component {
+export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -43,5 +43,3 @@ class Login extends React.Component {
         )
     }
 }
-
-export default Login;
