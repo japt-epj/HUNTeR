70dac8d15900e80a32126003692dd16944428796
SBI- <pascal.huerlimann@gmail.com>
2018-04-13 09:38:00 +0200

    Remove unnceccessary imports

--- backend/src/main/java/ch/japt/epj/api/ExerciseController.java
+++ backend/src/main/java/ch/japt/epj/api/ExerciseController.java
@@ -1,13 +1,8 @@
 package ch.japt.epj.api;
 
 import ch.japt.epj.model.TaskModel;
-import ch.japt.epj.model.data.Answer;
-import ch.japt.epj.model.data.Task;
 import ch.japt.epj.model.dto.ExerciseDto;
 import ch.japt.epj.model.dto.NewExerciseDto;
-import ch.japt.epj.repository.AnswerRepository;
-import ch.japt.epj.repository.ExerciseRepository;
-import org.modelmapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @Controller
