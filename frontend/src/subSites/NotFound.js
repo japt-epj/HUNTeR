c318287820f0ef11dcd8dc821b0fb70fd09548fb
AndiHSR <andi.hoerler@hsr.ch>
2018-04-15 03:05:37 +0200

    Change of device check and update of images

--- frontend/src/subSites/NotFound.js
+++ frontend/src/subSites/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-import {Image, Grid, Message} from 'semantic-ui-react';
+import {Grid, Image} from 'semantic-ui-react';
 
 import ErrorDesktop from '../images/404Desktop.png';
 import ErrorMobile from '../images/404Mobile.png';
@@ -9,13 +9,12 @@ import ErrorMobile from '../images/404Mobile.png';
 
 export default function () {
     return (
-        <Grid centered>
-            <Grid.Row>
-                <NavLink to="/">
-                    <Image
-                        src={/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? ErrorMobile : ErrorDesktop}/>
-                </NavLink>
-            </Grid.Row>
+        <Grid centered padded>
+            <NavLink to="/">
+                <Grid.Row>
+                    <Image src={window.innerWidth <= 768 ? ErrorMobile : ErrorDesktop}/>
+                </Grid.Row>
+            </NavLink>
         </Grid>
     )
 }
