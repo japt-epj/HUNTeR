f45863a4205cd3f87354b5572efa7d9bc20d236e
SBI- <pascal.huerlimann@gmail.com>
2018-04-16 21:06:08 +0200

    Handle all exercises with stream

--- backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
+++ backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
@@ -1,10 +1,15 @@
 package ch.japt.epj.repository;
 
 import ch.japt.epj.model.data.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public interface ExerciseRepository extends CrudRepository<Task, Long> {
     Optional<Task> findByTaskId(Long taskId);
+
+    @Query("select t from Task t")
+    Stream<Task> getAll();
 }
