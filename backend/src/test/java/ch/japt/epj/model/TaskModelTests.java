33ece94e00d0635d90e02638b4bfa5e2f8918510
SBI- <pascal.huerlimann@gmail.com>
2018-04-16 21:41:35 +0200

    Improve add task test

--- backend/src/test/java/ch/japt/epj/model/TaskModelTests.java
+++ backend/src/test/java/ch/japt/epj/model/TaskModelTests.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,16 +32,19 @@ public class TaskModelTests {
 
     @Test
     public void shouldAddNewTask() {
-        assertThat(model.getExercise(11l)).isEqualTo(Optional.empty());
+        int size = model.allExercises().size();
+        model.addExercise(makeTestDto());
+        assertThat(model.getExercise(size + 1l)).isNotEmpty();
+        assertThat(model.allExercises().size()).isEqualTo(size + 1);
+    }
+
+    private NewExerciseDto makeTestDto() {
         NewAnswerDto yes = new NewAnswerDto().text("Yes").checked(true);
         NewAnswerDto no = new NewAnswerDto().text("No").checked(false);
 
-        NewExerciseDto dto = new NewExerciseDto()
+        return new NewExerciseDto()
                 .title("Unit Test Question")
                 .question("Is this a unit test?")
                 .answers(Arrays.asList(new NewAnswerDto[]{yes, no}));
-
-        model.addExercise(dto);
-        assertThat(model.getExercise(11l)).isNotEmpty();
     }
 }
