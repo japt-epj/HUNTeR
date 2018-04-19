f26d1666935ec89ce7080820f217db81aef242d8
SBI- <pascal.huerlimann@gmail.com>
2018-04-19 19:58:59 +0200

    Revert pagination repository change

--- backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
+++ backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
@@ -1,16 +1,13 @@
 package ch.japt.epj.repository;
 
 import ch.japt.epj.model.data.Exercise;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public interface ExerciseRepository extends PagingAndSortingRepository<Exercise, Long> {
+public interface ExerciseRepository extends CrudRepository<Exercise, Long> {
     Optional<Exercise> findByExerciseId(Long exerciseId);
 
     @Query("select t from Exercise t")
