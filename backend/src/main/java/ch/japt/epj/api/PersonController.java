5baac023a4f57788a91c6460e2773a3643b17b25
TobiHSR <tobias.saladin@hsr.ch>
2018-04-19 14:38:02 +0200

    change user to person

---
+++ backend/src/main/java/ch/japt/epj/api/PersonController.java
@@ -0,0 +1,54 @@
+package ch.japt.epj.api;
+import ch.japt.epj.model.PersonModel;
+import ch.japt.epj.model.dto.PersonDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api")
+public class PersonController implements ch.japt.epj.api.PersonApi{
+    private final PersonModel personModel;
+
+    public PersonController(@Autowired PersonModel personModel) { this.personModel = personModel; }
+
+
+    @Override
+    public ResponseEntity<Void> createperson(PersonDto body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteperson(String personname) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<PersonDto> getpersonByName(String personname) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> loginperson(String personname, String password) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> logoutperson() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<PersonDto>> personGet() {
+        return new ResponseEntity<>(personModel.allPersons(), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> updateperson(String personname, PersonDto body) {
+        return null;
+    }
+}
