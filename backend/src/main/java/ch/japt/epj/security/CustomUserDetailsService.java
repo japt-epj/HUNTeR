845e02c56c85b99436685da6fda0e4470357dc9e
Jonas Kugler <jonas.kugler@hsr.ch>
2018-04-20 10:33:18 +0200

    Add CustomUserDetails and CustomUserDetailsService to security package

--- backend/src/main/java/ch/japt/epj/security/CustomUserDetailsService.java
+++ backend/src/main/java/ch/japt/epj/security/CustomUserDetailsService.java
@@ -1,4 +1,27 @@
 package ch.japt.epj.security;
 
-public class CustomUserDetailsService {
+import ch.japt.epj.model.data.Person;
+import ch.japt.epj.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private final PersonRepository repository;
+
+    public CustomUserDetailsService(@Autowired PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Person person = repository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found."));
+        return CustomUserDetails.create(person);
+    }
 }
