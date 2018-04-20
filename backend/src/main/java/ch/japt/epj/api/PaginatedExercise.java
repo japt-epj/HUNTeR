dc22a82fa82c9518d93bb13e5360f075fe7a2f1c
SBI- <pascal.huerlimann@gmail.com>
2018-04-20 09:18:41 +0200

    Replace generated swagger api with interface

--- backend/src/main/java/ch/japt/epj/api/PaginatedExercise.java
+++ backend/src/main/java/ch/japt/epj/api/PaginatedExercise.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +15,14 @@ import javax.validation.Valid;
 import java.util.List;
 
 public interface PaginatedExercise {
-//    @ApiOperation(value = "Get all exercises", nickname = "exerciseGet", notes = "", response = ExerciseDto.class, responseContainer = "List", tags={ "exercise", })
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "All exercises", response = ExerciseDto.class, responseContainer = "List") })
-//    @RequestMapping(value = "/exercise",
-//            produces = { "application/json" },
-//            method = RequestMethod.GET)
-//    ResponseEntity<List<ExerciseDto>> exerciseGet(
-//            @ApiParam(value = "Page index from where to start pagination") @Valid @RequestParam(value = "page", required = false) Integer page,
-//            @ApiParam(value = "Number of pages to fetch") @Valid @RequestParam(value = "limit", required = false) Integer limit);
+    @ApiOperation(value = "Get all exercises", nickname = "exerciseGet", notes = "", response = ExerciseDto.class, responseContainer = "List", tags={ "exercise", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "All exercises", response = ExerciseDto.class, responseContainer = "List") })
+    @RequestMapping(value = "/exercise",
+            produces = { "application/json" },
+            method = RequestMethod.GET)
+    ResponseEntity<Page<ExerciseDto>> exerciseGet(
+            @ApiParam(value = "Page index from where to start pagination") @Valid @RequestParam(value = "first", required = false) Integer first,
+            @ApiParam(value = "Number of pages to fetch") @Valid @RequestParam(value = "limit", required = false) Integer limit);
 
 }
