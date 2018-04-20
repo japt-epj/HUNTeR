8441845fbf27f7766d303ec37524a8a04a9b8d73
AndiHSR <andi.hoerler@hsr.ch>
2018-04-20 06:49:53 +0200

    Implement POST function for quiz datas

--- frontend/src/subSites/teacher/TeacherQuiz.js
+++ frontend/src/subSites/teacher/TeacherQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Redirect} from 'react-router';
 
-import {Button, Dimmer, Form, Grid, Loader, Modal, Table} from 'semantic-ui-react';
+import {Button, Dimmer, Form, Grid, Loader, Modal} from 'semantic-ui-react';
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import "react-day-picker/lib/style.css";
 import {formatDate} from 'react-day-picker/moment';
@@ -9,11 +10,13 @@ import ExerciseHandler from '../../handlers/ExerciseHandler';
 import APIHandler from '../../handlers/APIHandler';
 import StudentHandler from "../../handlers/StudentHandler";
 import TableHandler from "../../handlers/TableHandler";
+import FormHandler from "../../handlers/FormHandler";
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            title: '',
             exercises: [],
             selectedExercises: [],
             students: [],
@@ -28,7 +31,8 @@ export default class extends React.Component {
             menuNumber: 0,
             minPageNumber: 0,
             maxPageNumber: 10,
-            startDate: new Date()
+            endDate: new Date(),
+            fireRedirect: false
         };
         this.getExerciseTable = ExerciseHandler.getExerciseTable.bind(this);
         this.getStudentTable = StudentHandler.getStudentTable.bind(this);
@@ -39,6 +43,10 @@ export default class extends React.Component {
         this.handlePageChange = this.handlePageChange.bind(this);
         this.resetPageNumber = this.resetPageNumber.bind(this);
         this.handleDayChange = this.handleDayChange.bind(this);
+
+        this.handleSubmit = FormHandler.handleQuizSumbit.bind(this);
+        this.handleChange = FormHandler.handleChange.bind(this);
+        this.postData = APIHandler.postData.bind(this);
     }
 
     componentDidMount() {
@@ -61,7 +69,7 @@ export default class extends React.Component {
     }
 
     handleDayChange(day) {
-        this.setState({startDate: day});
+        this.setState({endDate: day});
     }
 
     handlePageChange(event, element) {
@@ -80,17 +88,19 @@ export default class extends React.Component {
 
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
                 <Grid>
                     <Grid.Row>
                         <Grid.Column>
-                            <Form.Input fluid label="Titel" placeholder="Bitte geben Sie einen Titel ein" required/>
+                            <Form.Input fluid label="Titel" name="title" value={this.state.title}
+                                        onChange={this.handleChange}
+                                        placeholder="Bitte geben Sie einen Titel ein" required/>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row columns="equal">
                         <Grid.Column>
                             <Form.Input label="Quiz endet am" inline>
-                                <DayPickerInput format="DD.MM.YYYY" formatDate={formatDate} value={this.state.startDate}
+                                <DayPickerInput format="DD.MM.YYYY" formatDate={formatDate} value={this.state.endDate}
                                                 onDayChange={this.handleDayChange}
                                                 dayPickerProps={{showWeekNumbers: true, todayButton: 'Heute'}}/>
                             </Form.Input>
@@ -132,6 +142,7 @@ export default class extends React.Component {
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
+                {this.state.fireRedirect && (<Redirect to="/"/>)}
             </Form>
         );
     }
