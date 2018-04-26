ed60fcd7ce066a2208ba252770b1625d8132f253
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-04-26 19:23:15 +0200

    Remove unneded modal function

--- frontend/src/handlers/ModalHandler.js
+++ frontend/src/handlers/ModalHandler.js
@@ -1,25 +1,8 @@
 import React from 'react';
 
-import {Button, Icon, Modal} from 'semantic-ui-react';
+import {} from 'semantic-ui-react';
 
 
 export default class ModalHandler {
-    static getDeleteModal(element) {
-        return (
-            <Modal size="fullscreen"
-                   trigger={<Button basic negative content={element.buttonContent} icon="delete"/>}
-                   closeIcon>
-                <Modal.Header>element.title</Modal.Header>
-                <Modal.Content>
-                    <Modal.Description>
-                        element.description
-                    </Modal.Description>
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button negative icon="cancel" content="Nicht löschen"/>
-                    <Button positive icon="checkmark" content="Löschen"/>
-                </Modal.Actions>
-            </Modal>
-        );
-    }
+
 }
