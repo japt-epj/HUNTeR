58e34084a52b719b3a9f950375a16c970f5bd3fc
Jonas Kugler <jonas.kugler@hsr.ch>
2018-04-27 16:25:55 +0200

    Add RegPersonModel

--- backend/src/main/java/ch/japt/epj/model/RegPersonModel.java
+++ backend/src/main/java/ch/japt/epj/model/RegPersonModel.java
@@ -1,4 +1,42 @@
-package ch.japt.epj.model.data;
+package ch.japt.epj.model;
 
+import ch.japt.epj.model.data.Person;
+import ch.japt.epj.model.data.Role;
+import ch.japt.epj.model.data.RoleName;
+import ch.japt.epj.model.dto.RegPersonDto;
+import ch.japt.epj.repository.PersonRepository;
+import ch.japt.epj.repository.RoleRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
 public class RegPersonModel {
+
+    private final PersonRepository persons;
+    private final RoleRepository roles;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper mapper = new ModelMapper();
+
+    public RegPersonModel(
+            @Autowired PersonRepository persons,
+            @Autowired RoleRepository roles,
+            @Autowired PasswordEncoder passwordEncoder
+    ){
+        this.persons = persons;
+        this.roles = roles;
+        this.passwordEncoder = passwordEncoder;
+        mapper.createTypeMap(Person.class, RegPersonDto.class);
+    }
+
+    public void addPerson(RegPersonDto regPersonDto){
+        Person person = mapper.map(regPersonDto, Person.class);
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        Role personRole = roles.findByName(RoleName.ROLE_TEACHER).orElseThrow(() -> new IllegalArgumentException("Unable to assign teacher role to person."));
+        person.setRoles(Collections.singleton(personRole));
+        persons.save(person);
+    }
 }
