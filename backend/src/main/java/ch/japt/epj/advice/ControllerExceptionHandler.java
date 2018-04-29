d6000cd480e6efc46bef702a3a1e8272c114725f
SBI- <pascal.huerlimann@gmail.com>
2018-04-29 18:49:41 +0200

    Add simple implementation of array id query

--- backend/src/main/java/ch/japt/epj/advice/ControllerExceptionHandler.java
+++ backend/src/main/java/ch/japt/epj/advice/ControllerExceptionHandler.java
@@ -1,7 +1,6 @@
 package ch.japt.epj.advice;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
