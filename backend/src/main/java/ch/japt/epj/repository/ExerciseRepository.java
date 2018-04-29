23ddc97e5d473f93cbc1e467e7d67d9086baa645
SBI- <pascal.huerlimann@gmail.com>
2018-04-29 14:56:44 +0200

    Move custom implementations to separate namespace

--- backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
+++ backend/src/main/java/ch/japt/epj/repository/ExerciseRepository.java
@@ -1,6 +1,7 @@
 package ch.japt.epj.repository;
 
 import ch.japt.epj.model.data.Exercise;
+import ch.japt.epj.repository.custom.ExerciseRepositoryCustom;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
