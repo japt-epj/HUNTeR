1209ac5e3dc4d8130700e58076b0cfb0e9d221c1
SBI- <pascal.huerlimann@gmail.com>
2018-04-29 14:51:27 +0200

    Demonstrate how extending a repository can work

---
+++ backend/src/main/java/ch/japt/epj/repository/ExerciseRepositoryCustom.java
@@ -0,0 +1,9 @@
+package ch.japt.epj.repository;
+
+import ch.japt.epj.model.data.Exercise;
+
+import java.util.List;
+
+public interface ExerciseRepositoryCustom {
+    List<Exercise> weirdAssFunctionName(List<Integer> ids);
+}
