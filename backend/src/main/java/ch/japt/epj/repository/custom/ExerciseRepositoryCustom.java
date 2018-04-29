ee890a8be0b4b8acafdb8df5b10914b91abf740c
SBI- <pascal.huerlimann@gmail.com>
2018-04-29 19:02:16 +0200

    Clean up overly complicated solution

--- backend/src/main/java/ch/japt/epj/repository/custom/ExerciseRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package ch.japt.epj.repository.custom;
-
-import ch.japt.epj.model.data.Exercise;
-
-import java.util.List;
-
-public interface ExerciseRepositoryCustom {
-    List<Exercise> findByIds(List<Integer> ids);
-}
