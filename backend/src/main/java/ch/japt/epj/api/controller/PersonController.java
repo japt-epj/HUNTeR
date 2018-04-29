b3a181c4720f31bc9042b256878017b3e65c22e3
SBI- <pascal.huerlimann@gmail.com>
2018-04-29 21:11:17 +0200

    Implement 'people' endpoint for multiple people

--- backend/src/main/java/ch/japt/epj/api/controller/PersonController.java
+++ backend/src/main/java/ch/japt/epj/api/controller/PersonController.java
@@ -1,63 +1,38 @@
 package ch.japt.epj.api.controller;
 import ch.japt.epj.api.PaginatedPerson;
+import ch.japt.epj.api.PeopleApi;
+import ch.japt.epj.api.PersonApi;
 import ch.japt.epj.library.SortParameterHandler;
 import ch.japt.epj.model.PersonModel;
-import ch.japt.epj.model.data.Person;
-import ch.japt.epj.model.data.Role;
-import ch.japt.epj.model.data.RoleName;
 import ch.japt.epj.model.dto.PersonDto;
-import ch.japt.epj.repository.PersonRepository;
-import ch.japt.epj.repository.RoleRepository;
-import ch.japt.epj.security.JwtTokenProvider;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import java.net.URI;
-import java.util.Collections;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 @Api(tags = "Person API")
 @RequestMapping("/api")
-public class PersonController implements ch.japt.epj.api.PersonApi, PaginatedPerson{
+public class PersonController implements PersonApi, PeopleApi, PaginatedPerson {
 
     private final PersonModel personModel;
 
-    public PersonController(
-            @Autowired PersonModel personModel
-    ) {
+    public PersonController(@Autowired PersonModel personModel) {
         this.personModel = personModel;
     }
-
-
-
-//
-//    @Override
-//    public ResponseEntity<Void> deletePerson(String email) {
-//        return null;
-//    }
 
     @Override
     public ResponseEntity<PersonDto> getPersonByEmail(String email) {
         return null;
     }
-
-
-
 
     @Override
     public ResponseEntity<Void> updatePerson(String personname, PersonDto body) {
@@ -72,5 +47,10 @@ public class PersonController implements ch.japt.epj.api.PersonApi, PaginatedPer
         return new ResponseEntity<>(
                 personModel.pagePeople(page, limit, SortParameterHandler.makeSort(sortOptions)),
                 HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<PersonDto>> getPersonById(@Valid @PathVariable("id") List<Integer> id) {
+        return new ResponseEntity<>(personModel.getPeople(id), HttpStatus.OK);
     }
 }
