38ac693dad27c4bc02402afaea1182b77e65acfa
Jonas Kugler <jonas.kugler@hsr.ch>
2018-05-03 16:50:48 +0200

    Fix a problem causing empty response bodies

--- backend/src/main/java/ch/japt/epj/security/CustomUserDetailsService.java
+++ backend/src/main/java/ch/japt/epj/security/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -20,11 +21,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Person person = repository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found."));
         return CustomUserDetails.create(person);
     }
-
+    @Transactional
     public UserDetails loadUserById(Long id) {
         Person person = repository.findByPersonId(id).orElseThrow(() -> new UsernameNotFoundException("User with id : " + id + " not found."));
         return CustomUserDetails.create(person);
