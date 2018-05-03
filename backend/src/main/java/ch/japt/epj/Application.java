0ba8734a02e3bbb27c08b3eed6e30d288c9a6eab
SBI- <pascal.huerlimann@gmail.com>
2018-05-03 11:18:36 +0200

    Add basic logging configuration and replace sout

--- backend/src/main/java/ch/japt/epj/Application.java
+++ backend/src/main/java/ch/japt/epj/Application.java
@@ -2,6 +2,8 @@ package ch.japt.epj;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -18,13 +22,12 @@ public class Application {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            log.info("All included beans:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
-                System.out.println(beanName);
+                log.info(beanName);
             }
 
         };
