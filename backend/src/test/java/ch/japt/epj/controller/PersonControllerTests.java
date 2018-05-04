1e96ac469bc21fde230b93034996071263138e1e
SBI- <pascal.huerlimann@gmail.com>
2018-05-04 16:54:44 +0200

    Add basic person test

--- backend/src/test/java/ch/japt/epj/controller/PersonControllerTests.java
+++ backend/src/test/java/ch/japt/epj/controller/PersonControllerTests.java
@@ -5,10 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,8 +25,20 @@ public class PersonControllerTests {
     private MockMvc mvc;
 
     @Test
-    private void getExistingPersonById() {
-        MockMvcRequestBuilders
-                .get("/api/person/")
+    public void getExistingPersonById() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/api/person/2")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0]").exists())
+                .andExpect(jsonPath("$[0].firstName").value("Pascal"))
+                .andExpect(jsonPath("$[0].lastName").value("Hürlimann"))
+                .andExpect(jsonPath("$[0].email").value("pascal.huerlimann@hsr.ch"));
+                // this needs to be uncommented, we really shouldn't be sending passwords!
+//                .andExpect(jsonPath("$[0].password").doesNotExist());
+
     }
 }
