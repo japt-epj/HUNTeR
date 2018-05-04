9d1f06de560137883a2eacffebe4f50175dc8c9f
SBI- <pascal.huerlimann@gmail.com>
2018-05-04 09:42:37 +0200

    Add more exercise controller integration tests

--- backend/src/test/java/ch/japt/epj/controller/ExerciseControllerTests.java
+++ backend/src/test/java/ch/japt/epj/controller/ExerciseControllerTests.java
@@ -40,6 +40,59 @@ public class ExerciseControllerTests {
     }
 
     @Test
+    public void checkPageInformationDefaultQuery() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/api/exercise")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.numberOfElements").value(5))
+                .andExpect(jsonPath("$.size").value(5))
+                .andExpect(jsonPath("$.number").value(0))
+                .andExpect(jsonPath("$.last").value(false))
+                .andExpect(jsonPath("$.first").value(true))
+                .andExpect(jsonPath("$.sort[0].direction").value("ASC"))
+                .andExpect(jsonPath("$.sort[0].property").value("name"))
+                .andExpect(jsonPath("$.sort[0].ascending").value("true"))
+                .andExpect(jsonPath("$.sort[0].descending").value("false"));
+
+    }
+
+    @Test
+    public void checkPageInformationEmptyPage() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/api/exercise?page=100&limit=2")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.numberOfElements").value(0))
+                .andExpect(jsonPath("$.size").value(2))
+                .andExpect(jsonPath("$.number").value(100))
+                .andExpect(jsonPath("$.last").value(true))
+                .andExpect(jsonPath("$.first").value(false))
+                .andExpect(jsonPath("$.sort[0].direction").value("ASC"))
+                .andExpect(jsonPath("$.sort[0].property").value("name"))
+                .andExpect(jsonPath("$.sort[0].ascending").value("true"))
+                .andExpect(jsonPath("$.sort[0].descending").value("false"));
+
+    }
+
+    @Test
+    public void checkInvalidPagination() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get("/api/exercise/?sort=blabla")
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    @Test
     public void getExerciseNotFound() throws Exception {
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
                 .get("/api/exercise/100000")
@@ -47,6 +100,31 @@ public class ExerciseControllerTests {
 
         mvc.perform(request)
                 .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().string("[]"));
+    }
+
+    @Test
+    public void makeExerciseSuccess() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/api/exercise")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"name\": \"This is a test question\", \"question\": \"What is 42?\", \"answers\": [ { \"text\": \"A number\", \"checked\": \"false\" }, { \"text\": \"The answer to everything\", \"checked\": \"true\" } ] }");
+
+        mvc.perform(request)
+                // this should be set and needs to be fixed on the api level
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void makeExerciseFailure() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post("/api/exercise")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}");
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
     }
 }
