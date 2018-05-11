089594c5262707cb13f5cb092e021a40211b1aa0
Jonas Kugler <jonas.kugler@hsr.ch>
2018-05-11 13:10:03 +0200

    Reformat code

--- backend/src/main/java/ch/japt/epj/configuration/WebSecurityConfig.java
+++ backend/src/main/java/ch/japt/epj/configuration/WebSecurityConfig.java
@@ -20,81 +20,77 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-    @Autowired
-    JwtTokenProvider tokenProvider;
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
+  @Autowired
+  JwtTokenProvider tokenProvider;
+  @Autowired
+  CustomUserDetailsService customUserDetailsService;
 
 
-    private final JwtAuthenticationEntryPoint unauthorizedHandler;
+  private final JwtAuthenticationEntryPoint unauthorizedHandler;
 
-    public WebSecurityConfig(@Autowired JwtAuthenticationEntryPoint unauthorizedHandler, @Autowired CustomUserDetailsService customUserDetailsService) {
-        this.unauthorizedHandler = unauthorizedHandler;
-        this.customUserDetailsService = customUserDetailsService;
-    }
+  public WebSecurityConfig(@Autowired JwtAuthenticationEntryPoint unauthorizedHandler,
+      @Autowired CustomUserDetailsService customUserDetailsService) {
+    this.unauthorizedHandler = unauthorizedHandler;
+    this.customUserDetailsService = customUserDetailsService;
+  }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
+  @Override
+  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder)
+      throws Exception {
+    authenticationManagerBuilder
+        .userDetailsService(customUserDetailsService)
+        .passwordEncoder(passwordEncoder());
+  }
 
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  @Bean(BeanIds.AUTHENTICATION_MANAGER)
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // these paths are allowed free access
-        String[] allowed = {
-//                "/",
-//              "/*",
-//              "/**",
-//              "/static/**/*",
-                "/api/auth/**"
-        };
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // these paths are allowed free access
+    String[] allowed = {
+        "/api/auth/**"
+    };
 
-        http
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(unauthorizedHandler)
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers(allowed)
-                .permitAll()
-//                  .antMatchers(HttpMethod.GET, "/api/**")
-//              .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
+    http
+        .cors()
+        .and()
+        .csrf()
+        .disable()
+        .exceptionHandling()
+        .authenticationEntryPoint(unauthorizedHandler)
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers(allowed)
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
 //                uncommenting this should enable a default login form
 //                we need to replace this with a custom one
-                    .loginPage("/")
-                    .permitAll()
-//                    .successHandler(successHandler)
-                    .and()
-                .logout()
-                .permitAll();
+        .loginPage("/")
+        .permitAll()
+        .and()
+        .logout()
+        .permitAll();
 //
 //        // This is key for exposing csrf tokens in apis that are outside
 //        // of the browser. We will need these headers in react and for
 //        // testing with postman etc.
-        http.addFilterBefore(new JwtAuthenticationFilter(tokenProvider, customUserDetailsService), UsernamePasswordAuthenticationFilter.class);
-    }
+    http.addFilterBefore(new JwtAuthenticationFilter(tokenProvider, customUserDetailsService),
+        UsernamePasswordAuthenticationFilter.class);
+  }
 }
