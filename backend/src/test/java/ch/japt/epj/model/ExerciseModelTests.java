2131f4839ff86ea2f13e79db91a688f8da5d7b25
Jonas Kugler <jonas.kugler@hsr.ch>
2018-05-11 10:48:17 +0200

    Fix problem with missing mvcHandlerMappingIntrospecter Bean

--- backend/src/test/java/ch/japt/epj/model/ExerciseModelTests.java
+++ backend/src/test/java/ch/japt/epj/model/ExerciseModelTests.java
@@ -9,8 +9,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
@@ -23,8 +26,12 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest(includeFilters = @ComponentScan.Filter(Component.class))
+//@DataJpaTest(includeFilters = @ComponentScan.Filter(Component.class))
+@SpringBootTest
+@AutoConfigureTestDatabase
 public class ExerciseModelTests {
+
+
     @Autowired
     private ExerciseModel model;
 
