7c9325d93b6586ef8e3b17916c95d80eb07827fd
Jonas Kugler <jonas.kugler@hsr.ch>
2018-05-11 13:10:03 +0200

    Reformat code

--- backend/src/main/java/ch/japt/epj/api/controller/AuthController.java
+++ backend/src/main/java/ch/japt/epj/api/controller/AuthController.java
@@ -34,84 +34,84 @@ import java.util.Collection;
 @RequestMapping("/api")
 public class AuthController implements ch.japt.epj.api.AuthApi {
 
-    private final RegPersonModel regPersonModel;
-    private final AuthenticationManager authenticationManager;
-    private final PersonRepository personRepository;
-    private final JwtTokenProvider tokenProvider;
+  private final RegPersonModel regPersonModel;
+  private final AuthenticationManager authenticationManager;
+  private final PersonRepository personRepository;
+  private final JwtTokenProvider tokenProvider;
 
 
-    public AuthController(
-            @Autowired RegPersonModel regPersonModel,
-            @Autowired AuthenticationManager authenticationManager,
-            @Autowired PersonRepository personRepository,
-            @Autowired JwtTokenProvider tokenProvider
-    ) {
-        this.regPersonModel = regPersonModel;
-        this.authenticationManager = authenticationManager;
-        this.personRepository = personRepository;
-        this.tokenProvider = tokenProvider;
+  public AuthController(
+      @Autowired RegPersonModel regPersonModel,
+      @Autowired AuthenticationManager authenticationManager,
+      @Autowired PersonRepository personRepository,
+      @Autowired JwtTokenProvider tokenProvider
+  ) {
+    this.regPersonModel = regPersonModel;
+    this.authenticationManager = authenticationManager;
+    this.personRepository = personRepository;
+    this.tokenProvider = tokenProvider;
+  }
+
+  @Override
+  public ResponseEntity<JWTDto> loginPerson(@Valid @RequestBody AuthPersonDto body) {
+    Authentication authentication = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword())
+    );
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    String jwt = tokenProvider.generateToken(authentication);
+    JWTDto dto = new JWTDto();
+    dto.setToken(jwt);
+    dto.setTokenType("Bearer");
+    return new ResponseEntity<>(dto, HttpStatus.OK);
+  }
+
+  @Override
+  public ResponseEntity<Void> registerPerson(@Valid @RequestBody RegPersonDto body) {
+
+    if (personRepository.existsByEmail(body.getEmail())) {
+      return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
-    @Override
-    public ResponseEntity<JWTDto> loginPerson(@Valid @RequestBody AuthPersonDto body) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword())
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+    regPersonModel.addPerson(body);
 
-        String jwt = tokenProvider.generateToken(authentication);
-        JWTDto dto = new JWTDto();
-        dto.setToken(jwt);
-        dto.setTokenType("Bearer");
-        return new ResponseEntity<>(dto, HttpStatus.OK);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @Override
+  public ResponseEntity<Void> getEntryPoint(@RequestHeader("X-HUNTeR-Frontend") Boolean hunter) {
+    HttpHeaders headers = new HttpHeaders();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    boolean isStudent = false;
+    boolean isTeacher = false;
+
+    for (GrantedAuthority grantedAuthority : authorities) {
+      if (grantedAuthority.getAuthority().equals("ROLE_TEACHER")) {
+        isTeacher = true;
+      } else if (grantedAuthority.getAuthority().equals("ROLE_STUDENT")) {
+        isStudent = true;
+      }
     }
 
-    @Override
-    public ResponseEntity<Void> registerPerson(@Valid @RequestBody RegPersonDto body) {
-
-        if (personRepository.existsByEmail(body.getEmail())) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }
-
-        regPersonModel.addPerson(body);
-
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    if (hunter) {
+      if (isTeacher) {
+        headers.add("X-HUNTeR-Redirect", "/teacher");
+      } else if (isStudent) {
+        headers.add("X-HUNTeR-Redirect", "/participant");
+      } else {
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+      }
+      return new ResponseEntity<>(headers, HttpStatus.OK);
+    } else {
+      if (isTeacher) {
+        headers.add("Location", "/teacher");
+      } else if (isStudent) {
+        headers.add("Location", "/participant");
+      } else {
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+      }
+      return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
-
-    @Override
-    public ResponseEntity<Void> getEntryPoint(@RequestHeader("X-HUNTeR-Frontend") Boolean hunter) {
-        HttpHeaders headers = new HttpHeaders();
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        boolean isStudent = false;
-        boolean isTeacher = false;
-
-        for (GrantedAuthority grantedAuthority : authorities){
-            if (grantedAuthority.getAuthority().equals("ROLE_TEACHER")) {
-                isTeacher = true;
-            }else if (grantedAuthority.getAuthority().equals("ROLE_STUDENT")) {
-                isStudent = true;
-            }
-        }
-
-        if (hunter) {
-            if (isTeacher) {
-                headers.add("X-HUNTeR-Redirect", "/teacher");
-            } else if(isStudent) {
-                headers.add("X-HUNTeR-Redirect", "/participant");
-            } else {
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-            return new ResponseEntity<>(headers, HttpStatus.OK);
-        } else {
-            if (isTeacher) {
-                headers.add("Location", "/teacher");
-            } else if(isStudent) {
-                headers.add("Location", "/participant");
-            } else {
-                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-            }
-            return new ResponseEntity<>(headers, HttpStatus.FOUND);
-        }
-    }
+  }
 }
--- backend/src/main/java/ch/japt/epj/security/CustomUserDetails.java
+++ backend/src/main/java/ch/japt/epj/security/CustomUserDetails.java
@@ -12,88 +12,93 @@ import java.util.stream.Collectors;
 
 public class CustomUserDetails implements UserDetails {
 
-    private long personId;
+  private long personId;
 
-    private String email;
+  private String email;
 
-    private String password;
+  private String password;
 
-    private Collection<? extends GrantedAuthority> authorities;
+  private Collection<? extends GrantedAuthority> authorities;
 
-    public CustomUserDetails(Long personId, String email, String password, Collection<? extends GrantedAuthority> authorities) {
-        this.personId = personId;
-        this.email = email;
-        this.password = password;
-        this.authorities = authorities;
-    }
+  public CustomUserDetails(Long personId, String email, String password,
+      Collection<? extends GrantedAuthority> authorities) {
+    this.personId = personId;
+    this.email = email;
+    this.password = password;
+    this.authorities = authorities;
+  }
 
-    public static CustomUserDetails create(Person person) {
-        List<GrantedAuthority> authorities = person.getRoles().stream().map(role ->
-                new SimpleGrantedAuthority(role.getName().name())
-        ).collect(Collectors.toList());
+  public static CustomUserDetails create(Person person) {
+    List<GrantedAuthority> authorities = person.getRoles().stream().map(role ->
+        new SimpleGrantedAuthority(role.getName().name())
+    ).collect(Collectors.toList());
 
-        return new CustomUserDetails(
-                person.getPersonId(),
-                person.getEmail(),
-                person.getPassword(),
-                authorities
-        );
-    }
+    return new CustomUserDetails(
+        person.getPersonId(),
+        person.getEmail(),
+        person.getPassword(),
+        authorities
+    );
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
 
-    public Long getPersonId() {
-        return personId;
-    }
+  public Long getPersonId() {
+    return personId;
+  }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+  @Override
+  public String getPassword() {
+    return password;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    @Override
-    public String getUsername() {
-        return null;
-    }
+  @Override
+  public String getUsername() {
+    return null;
+  }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomUserDetails that = (CustomUserDetails) o;
-        return Objects.equals(personId, that.personId);
-    }
+  @Override
+  public boolean equals(Object o) {
+      if (this == o) {
+          return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+          return false;
+      }
+    CustomUserDetails that = (CustomUserDetails) o;
+    return Objects.equals(personId, that.personId);
+  }
 
-    @Override
-    public int hashCode() {
+  @Override
+  public int hashCode() {
 
-        return Objects.hash(personId);
-    }
+    return Objects.hash(personId);
+  }
 }
