de83922f0042c47d89c48d3ab79439e17a7adc79
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-05-13 14:49:48 +0200

    Add posibility to update current position

--- frontend/src/subSites/participant/ParticipantNextLocation.js
+++ frontend/src/subSites/participant/ParticipantNextLocation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {Button, Grid} from 'semantic-ui-react';
 import L from 'leaflet';
 import {Map as LeafletMap, Marker, Tooltip, TileLayer} from 'react-leaflet';
 
@@ -25,9 +26,13 @@ export default class ParticipantNextLocation extends React.Component {
   }
 
   componentDidMount() {
-    this.mapref.current.leafletElement.locate();
+    this.locate();
     this.getNextLocation();
   }
+
+  locate = () => {
+    this.mapref.current.leafletElement.locate();
+  };
 
   getNextLocation = () => {
     //APIHandler.getNextLocation().then(resData => {
@@ -60,19 +65,24 @@ export default class ParticipantNextLocation extends React.Component {
   };
 
   handleLocation = event => {
-    const locations = [
-      {
+    let locations = [...this.state.locations];
+    if (locations[0].id === 'currentPosition') {
+      locations[0] = {
         id: 'currentPosition',
         title: 'Aktuelle Position',
         position: [event.latlng.lat, event.latlng.lng]
-      },
-      ...this.state.locations
-    ];
+      };
+    } else {
+      locations.unshift({
+        id: 'currentPosition',
+        title: 'Aktuelle Position',
+        position: [event.latlng.lat, event.latlng.lng]
+      });
+    }
 
     let map = {...this.state.map};
     map.zoom = this.mapref.current.leafletElement.getZoom();
     map.location = event.latlng;
-    console.log(event.latlng);
     map.clicked = false;
     this.setState({map, locations});
   };
@@ -83,6 +93,7 @@ export default class ParticipantNextLocation extends React.Component {
       iconSize: [50, 94],
       iconAnchor: [50, 0]
     });
+
     const protagonist = L.icon({
       iconUrl: require('../../images/icons/protagonist.png'),
       iconSize: [33, 92],
@@ -90,37 +101,47 @@ export default class ParticipantNextLocation extends React.Component {
     });
 
     return (
-      <div id="mapContainer">
-        <LeafletMap
-          center={this.state.map.location || [0, 0]}
-          onLocationFound={this.handleLocation}
-          zoom={this.state.map.zoom}
-          onZoomEnd={this.handleZoom}
-          ref={this.mapref}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {this.state.locations.map(element => (
-            <Marker
-              position={element.position}
-              icon={element.id === 'currentPosition' ? protagonist : pointer}
-              key={element.title + JSON.stringify(element.position)}
-            >
-              {element.title !== undefined && (
-                <Tooltip
-                  direction="left"
-                  offset={
-                    element.id === 'currentPosition' ? [-16, 0] : [-50, 75]
-                  }
-                  opacity={0.9}
-                  permanent
-                >
-                  <span>{element.title}</span>
-                </Tooltip>
-              )}
-            </Marker>
-          ))}
-        </LeafletMap>
-      </div>
+      <Grid padded>
+        <Grid.Row id="mapContainer">
+          <LeafletMap
+            center={this.state.map.location || [0, 0]}
+            onLocationFound={this.handleLocation}
+            zoom={this.state.map.zoom}
+            onZoomEnd={this.handleZoom}
+            ref={this.mapref}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {this.state.locations.map(element => (
+              <Marker
+                position={element.position}
+                icon={element.id === 'currentPosition' ? protagonist : pointer}
+                key={element.title + JSON.stringify(element.position)}
+              >
+                {element.title !== undefined && (
+                  <Tooltip
+                    direction="left"
+                    offset={
+                      element.id === 'currentPosition' ? [-16, 0] : [-50, 75]
+                    }
+                    opacity={0.9}
+                    permanent
+                  >
+                    <span>{element.title}</span>
+                  </Tooltip>
+                )}
+              </Marker>
+            ))}
+          </LeafletMap>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Button
+            positive
+            content={'Standort aktualisieren'}
+            icon="marker"
+            onClick={this.locate}
+          />
+        </Grid.Row>
+      </Grid>
     );
   }
 }
