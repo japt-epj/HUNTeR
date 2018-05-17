5e6ca9919d6dfdeffbceb8ecce85cee2b81cdb84
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-05-17 13:08:18 +0200

    Remove unused import

--- frontend/src/subSites/teacher/TeacherQuizOverview.js
+++ frontend/src/subSites/teacher/TeacherQuizOverview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-import {Button, Form} from 'semantic-ui-react';
+import {Button} from 'semantic-ui-react';
 import QuizHandler from '../../handlers/QuizHandler';
 import APIHandler from '../../handlers/APIHandler';
 import viewHandler from '../../handlers/viewHandler';
