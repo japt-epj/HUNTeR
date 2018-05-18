491be27c00c49d26ea5401dcca9064b3139f1e87
TobiHSR <tobias.saladin@hsr.ch>
2018-05-18 09:33:21 +0200

    First implementation for next location of exercise

--- backend/src/main/java/ch/japt/epj/model/LocationModel.java
+++ backend/src/main/java/ch/japt/epj/model/LocationModel.java
@@ -1,10 +1,15 @@
 package ch.japt.epj.model;
 
+import ch.japt.epj.library.LocationSorter;
+import ch.japt.epj.model.data.Exercise;
+import ch.japt.epj.model.data.Location;
+import ch.japt.epj.model.data.Response;
 import ch.japt.epj.model.dto.NextExerciseLocationDto;
 import ch.japt.epj.repository.LocationRepository;
 import ch.japt.epj.repository.QuizRepository;
 import ch.japt.epj.repository.ResponseRepository;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,32 +29,31 @@ public class LocationModel {
     this.responses = responses;
   }
 
-  public Optional<NextExerciseLocationDto> getExerciseLocation(Long id) {
+  public NextExerciseLocationDto getExerciseLocation(int id) {
     // TODO: Change static userId to user Id sent from frontend
-    //    ArrayList<Exercise> solvedExercises = responses.findByPersonId(1L)
-    //        .stream().map(Response::getExercise).collect(Collectors.toCollection(ArrayList::new));
-    //    ArrayList<Exercise> leftOverExercises = new ArrayList<>();
-    //    quizzes
-    //        .findQuizByQuizId(id)
-    //        .ifPresent(
-    //            quiz -> {
-    //              leftOverExercises.addAll(quiz.getTasks());
-    //              leftOverExercises.removeAll(solvedExercises);
-    //              leftOverExercises.forEach(e -> {
-    //                e.getLocations().removeAll(quiz.getLocations());
-    //                validExercises.put(e.getLocations().toArray()[0])
-    //              });
-    //
-    //            });
-    //
-    //    Collection<Exercise> sortedExercises =
-    // LocationSorter.nearestNeighborFromExercises(leftOverExercises);
-    //    Exercise nearestExercise = (Exercise)sortedExercises.toArray()[1];
-    //    NextExerciseLocationDto nextLocation = new NextExerciseLocationDto();
-    //    nextLocation.setExerciseTitle(nearestExercise.getExerciseTitle());
-    //    nextLocation.setLat(nearestExercise.get);
-    //    nextLocation.setLng(nearestExercise);
+    ArrayList<Exercise> solvedExercises =
+        responses
+            .findByPersonId(1L)
+            .stream()
+            .map(Response::getExercise)
+            .collect(Collectors.toCollection(ArrayList::new));
+    ArrayList<Location> allLocationsSorted = new ArrayList<>();
 
-    return null;
+    quizzes
+        .findQuizByQuizId((long) id)
+        .ifPresent(
+            quiz -> allLocationsSorted.addAll(LocationSorter.nearestNeighbor(quiz.getLocations())));
+    ArrayList<Location> filteredAndSortedLocations = new ArrayList<>(allLocationsSorted);
+    filteredAndSortedLocations.removeIf(
+        location -> solvedExercises.contains(location.getExercise()));
+
+    Location nextLocationToGo = filteredAndSortedLocations.get(0);
+    Exercise nextExerciseToDo = nextLocationToGo.getExercise();
+    NextExerciseLocationDto dto = new NextExerciseLocationDto();
+    dto.setExerciseTitle(nextExerciseToDo.getName());
+    dto.setLng(nextLocationToGo.getLng());
+    dto.setLat(nextLocationToGo.getLat());
+
+    return dto;
   }
 }
