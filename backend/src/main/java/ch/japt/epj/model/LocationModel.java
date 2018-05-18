5d2c88c2331cb84e91b1b687394df5b8ab26e666
TobiHSR <tobias.saladin@hsr.ch>
2018-05-18 12:28:10 +0200

    Add resopnses to mock data

--- backend/src/main/java/ch/japt/epj/model/LocationModel.java
+++ backend/src/main/java/ch/japt/epj/model/LocationModel.java
@@ -3,11 +3,14 @@ package ch.japt.epj.model;
 import ch.japt.epj.library.LocationSorter;
 import ch.japt.epj.model.data.Exercise;
 import ch.japt.epj.model.data.Location;
+import ch.japt.epj.model.data.Response;
 import ch.japt.epj.model.dto.NextExerciseLocationDto;
 import ch.japt.epj.repository.LocationRepository;
+import ch.japt.epj.repository.PersonRepository;
 import ch.japt.epj.repository.QuizRepository;
 import ch.japt.epj.repository.ResponseRepository;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,28 +20,33 @@ public class LocationModel {
   private final LocationRepository locations;
   private final QuizRepository quizzes;
   private final ResponseRepository responses;
+  private final PersonRepository persons;
 
   public LocationModel(
       @Autowired LocationRepository locations,
       @Autowired QuizRepository quizzes,
-      @Autowired ResponseRepository responses) {
+      @Autowired ResponseRepository responses,
+      @Autowired PersonRepository persons) {
     this.locations = locations;
     this.quizzes = quizzes;
     this.responses = responses;
+    this.persons = persons;
   }
 
   public NextExerciseLocationDto getExerciseLocation(int id) {
+    ArrayList<Exercise> solvedExercises = new ArrayList<>();
     // TODO: Change static userId to user Id sent from frontend
-    //    Response reponse = responses.getAll().(response -> response.getPerson().getPersonId() ==
-    // id);
-    ArrayList<Exercise> solvedExercises = null;
-    //        responses
-    //            .findByPersonId(1L)
-    //            .stream()
-    //            .map(Response::getExercise)
-    //            .collect(Collectors.toCollection(ArrayList::new));
+    persons
+        .findByPersonId(1L)
+        .ifPresent(
+            p ->
+                solvedExercises.addAll(
+                    responses
+                        .getAll()
+                        .filter(response -> response.getPerson().getPersonId() == p.getPersonId())
+                        .map(Response::getExercise)
+                        .collect(Collectors.toCollection(ArrayList::new))));
     ArrayList<Location> allLocationsSorted = new ArrayList<>();
-
     quizzes
         .findQuizByQuizId((long) id)
         .ifPresent(
@@ -53,7 +61,6 @@ public class LocationModel {
     dto.setExerciseTitle(nextExerciseToDo.getName());
     dto.setLng(nextLocationToGo.getLng());
     dto.setLat(nextLocationToGo.getLat());
-
     return dto;
   }
 }
