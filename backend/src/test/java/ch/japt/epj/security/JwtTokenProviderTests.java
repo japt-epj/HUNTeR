c14c028c8dcc8a7c71d876b32dcd9138d86bde2a
Jonas Kugler <jonas.kugler@hsr.ch>
2018-05-18 12:46:12 +0200

    Add tests for possible exceptions of TokenProvider

--- backend/src/test/java/ch/japt/epj/security/JwtTokenProviderTests.java
+++ backend/src/test/java/ch/japt/epj/security/JwtTokenProviderTests.java
@@ -1,5 +1,6 @@
 package ch.japt.epj.security;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -54,5 +55,32 @@ public class JwtTokenProviderTests {
     mvc.perform(
             MockMvcRequestBuilders.get("/api/person").header("Authorization", "Bearer " + token))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  public void shouldReturnFalseWithInvalidTokenSignature() throws Exception {
+    String token =
+        "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2p3dC1pZHAuZXhhbXBsZS5jb20iLCJzdWIiOiJtYWlsdG86bWlrZUBleGFtcGxlLmNvbSIsIm5iZiI6MTUyNjYzNTY0OCwiZXhwIjoxNTI2NjM5MjQ4LCJpYXQiOjE1MjY2MzU2NDgsImp0aSI6ImlkMTIzNDU2IiwidHlwIjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9yZWdpc3RlciJ9.DdUBpjoShYezUaOEbxmOfz7O0XwqfX8XyE4XACO6EF7q4ITNm4Q9fIHsHEzXTBMREFP8LRCDwbWzA7p8Qgxwlw";
+    assertThat(tokenProvider.validateToken(token)).isFalse();
+  }
+
+  @Test
+  public void shouldReturnFalseWithEmptyToken() throws Exception {
+    String token = "";
+    assertThat(tokenProvider.validateToken(token)).isFalse();
+  }
+
+  @Test
+  public void shouldReturnFalseWithMalformedToken() throws Exception {
+    String token =
+        "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2p3dC1pZHAuZXhhbXBsZS5jb20iLCJzdWIiOiJtYWlsdG86bWlrZUBleGFtcGxlLmNvbSIsIm5iZiI6MTUyNjYzOTU1OCwiZXhwIjoxNTI2NjQzMTU4LCJpYXQiOjE1MjY2Mzk1NTgsImp0aSI6ImlkMTIzNDU2IiwidHlwIjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9yZWdpc3RlciJ9.Pfs18Hm2aISZte4ZLR5dRN1xJHs6_Pp8JE7UTqQDkLb3tYK1O7zrYNMuJQAvOqaB-DRobPNnDW9z7uBJBCNAYQ";
+    assertThat(tokenProvider.validateToken(token)).isFalse();
+  }
+
+  @Test
+  public void shouldReturnFalseWithExpiredToken() throws Exception {
+    String token =
+        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNTI2NjM5OTk2LCJleHAiOjE1MjY2Mzk5OTd9.lAopehfqkH261MFWS4Y__WvfqRRmDq7wZv0MEuQdphlDyz6Ru7S0UuBrtmw0Ob01CyqoO_riA6ODdp9zbzkt8A";
+    assertThat(tokenProvider.validateToken(token)).isFalse();
   }
 }
