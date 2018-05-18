24842bc10d9c60f6eeed75c9e0c142f08a3b9744
SBI- <pascal.huerlimann@gmail.com>
2018-05-18 16:34:12 +0200

    Remove border of image for proper page generation

--- backend/src/main/java/ch/japt/epj/library/pdf/ExercisePage.java
+++ backend/src/main/java/ch/japt/epj/library/pdf/ExercisePage.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.util.Matrix;
@@ -16,16 +17,19 @@ public final class ExercisePage {
   public static void addPage(Exercise exercise, PDDocument document, byte[] qrcode)
       throws IOException {
     PDPage page = new PDPage();
+    document.addPage(page);
 
-    try (PDPageContentStream content = new PDPageContentStream(document, page)) {
+    try (PDPageContentStream content =
+        new PDPageContentStream(document, page, AppendMode.APPEND, false, true)) {
       content.beginText();
       content.setFont(PDType1Font.HELVETICA_BOLD, 26);
       Float center = Geometry.getCenter(page);
-      content.setTextMatrix(
+      Matrix matrix =
           Matrix.getTranslateInstance(
               center.x
                   - Geometry.getStringWidth(exercise.getName(), PDType1Font.HELVETICA_BOLD, 26) / 2,
-              page.getMediaBox().getHeight() - 26 - 10));
+              page.getMediaBox().getHeight() - 26 - 10);
+      content.setTextMatrix(matrix);
       content.showText(exercise.getName());
       content.endText();
 
@@ -36,8 +40,6 @@ public final class ExercisePage {
           center.y - image.getHeight() / 2,
           image.getWidth(),
           image.getHeight());
-
-      document.addPage(page);
     }
   }
 }
