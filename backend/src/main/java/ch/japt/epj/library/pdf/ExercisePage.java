5bf332fee36c86edc669a70c314988e47248ca1a
SBI- <pascal.huerlimann@gmail.com>
2018-05-20 18:11:46 +0200

    Extract functions for generating a page

--- backend/src/main/java/ch/japt/epj/library/pdf/ExercisePage.java
+++ backend/src/main/java/ch/japt/epj/library/pdf/ExercisePage.java
@@ -1,5 +1,6 @@
 package ch.japt.epj.library.pdf;
 
+import ch.japt.epj.library.QrGenerator;
 import ch.japt.epj.model.data.Exercise;
 import java.awt.geom.Point2D;
 import java.io.IOException;
@@ -16,51 +17,66 @@ import org.apache.pdfbox.util.Matrix;
 public final class ExercisePage {
   private ExercisePage() {}
 
-  public static void addPage(Exercise exercise, PDDocument document, byte[] qrcode)
+  private static void addTitle(String title, PDPageContentStream content, PDPage page)
       throws IOException {
+    Point2D.Float center = Geometry.getCenter(page);
+    content.beginText();
+    content.setFont(PDType1Font.HELVETICA_BOLD, 36);
+    Matrix matrix =
+        Matrix.getTranslateInstance(
+            center.x - Geometry.getStringWidth(title, PDType1Font.HELVETICA_BOLD, 36) / 2,
+            page.getMediaBox().getHeight() - 36 - 20);
+    content.setTextMatrix(matrix);
+    content.showText(title);
+    content.endText();
+  }
+
+  private static void addQuestion(String question, PDPageContentStream content, PDPage page)
+      throws IOException {
+    Point2D.Float center = Geometry.getCenter(page);
+    float em = Geometry.getStringWidth("M", PDType1Font.HELVETICA, 20);
+    float letters = page.getMediaBox().getWidth() / em * 1.5F;
+
+    Collection<String> lines = StringSplit.lines(question, Math.round(letters));
+
+    // set initial line to start bellow the title
+    int lineCount = 5;
+
+    for (String line : lines) {
+      content.beginText();
+      content.setFont(PDType1Font.HELVETICA, 20);
+      content.newLineAtOffset(
+          center.x - Geometry.getStringWidth(line, PDType1Font.HELVETICA, 20) / 2,
+          page.getMediaBox().getHeight() - 20 * lineCount);
+      lineCount += 1;
+      content.showText(line);
+      content.endText();
+    }
+  }
+
+  private static void addImage(
+      Exercise exercise, PDDocument document, PDPageContentStream content, PDPage page)
+      throws IOException {
+    Point2D.Float center = Geometry.getCenter(page);
+    byte[] qrcode = QrGenerator.makeQr(String.valueOf(exercise.getExerciseId()), 20, 0).get();
+    PDImageXObject image = PDImageXObject.createFromByteArray(document, qrcode, null);
+    content.drawImage(
+        image,
+        center.x - image.getWidth() / 2,
+        center.y - image.getHeight() / 2,
+        image.getWidth(),
+        image.getHeight());
+  }
+
+  public static void addPage(Exercise exercise, PDDocument document) throws IOException {
     PDPage page = new PDPage(PDRectangle.A4);
     document.addPage(page);
 
     try (PDPageContentStream content =
         new PDPageContentStream(document, page, AppendMode.APPEND, false, true)) {
-      content.beginText();
-      content.setFont(PDType1Font.HELVETICA_BOLD, 36);
-      Point2D.Float center = Geometry.getCenter(page);
-      Matrix matrix =
-          Matrix.getTranslateInstance(
-              center.x
-                  - Geometry.getStringWidth(exercise.getName(), PDType1Font.HELVETICA_BOLD, 36) / 2,
-              page.getMediaBox().getHeight() - 36 - 20);
-      content.setTextMatrix(matrix);
-      content.showText(exercise.getName());
-      content.endText();
-
-      float em = Geometry.getStringWidth("M", PDType1Font.HELVETICA, 20);
-      float letters = page.getMediaBox().getWidth() / em * 1.5F;
-
-      Collection<String> lines = StringSplit.lines(exercise.getQuestion(), Math.round(letters));
-
-      // set initial line to start bellow the title
-      int lineCount = 5;
-
-      for (String line : lines) {
-        content.beginText();
-        content.setFont(PDType1Font.HELVETICA, 20);
-        content.newLineAtOffset(
-            center.x - Geometry.getStringWidth(line, PDType1Font.HELVETICA, 20) / 2,
-            page.getMediaBox().getHeight() - 20 * lineCount);
-        lineCount += 1;
-        content.showText(line);
-        content.endText();
-      }
-
-      PDImageXObject image = PDImageXObject.createFromByteArray(document, qrcode, null);
-      content.drawImage(
-          image,
-          center.x - image.getWidth() / 2,
-          center.y - image.getHeight() / 2,
-          image.getWidth(),
-          image.getHeight());
+      addTitle(exercise.getName(), content, page);
+      addQuestion(exercise.getQuestion(), content, page);
+      addImage(exercise, document, content, page);
     }
   }
 }
