593f2f5e4fea9f4cec161cbb0886f0acbe4b5c0e
AndiHSR <andi.hoerler@hsr.ch>
2018-05-20 16:03:00 +0200

    Create credential component for different credential types

--- frontend/cypress/integration/test-login.js
+++ frontend/cypress/integration/test-login.js
@@ -1,16 +1,11 @@
 import setViewport from '../helpers/setViewport';
+import Credentials from '../helpers/Credentials';
 import login from '../helpers/login';
 
 describe('Login tests', function() {
   it('Login test with wrong credentials', function() {
     setViewport('HD+');
-    const loginCredentials = {
-      email: 'andi.hoerler@hsr.ch',
-      password: 'Andi',
-      credentialsCorrect: false,
-      role: 'participant'
-    };
-    login(loginCredentials);
+    login(Credentials.getWrongCredentials());
     cy.url().should('equal', Cypress.env('baseUrl') + '/');
     cy
       .contains('Username oder Passwort falsch eingegeben')
@@ -19,32 +14,28 @@ describe('Login tests', function() {
 
   it('Login test with participant credentials', function() {
     setViewport('fullHD');
-    const loginCredentials = {
-      email: 'andi.hoerler@hsr.ch',
-      password: 'andi',
-      credentialsCorrect: true,
-      role: 'participant'
-    };
-    login(loginCredentials);
+    login(Credentials.getParticipantCredentials());
     cy
       .url()
-      .should('equal', Cypress.env('baseUrl') + '/' + loginCredentials.role);
+      .should(
+        'equal',
+        Cypress.env('baseUrl') +
+          '/' +
+          Credentials.getParticipantCredentials().role
+      );
     cy.get('.pusher > .ui > [href="/participant/logout"] > .item').click();
     cy.url().should('equal', Cypress.env('baseUrl') + '/');
   });
 
   it('Login test with teacher credentials', function() {
     setViewport('fullHD');
-    const loginCredentials = {
-      email: 'tobias.saladin@hsr.ch',
-      password: 'tobias',
-      credentialsCorrect: true,
-      role: 'teacher'
-    };
-    login(loginCredentials);
+    login(Credentials.getTeacherCredentials());
     cy
       .url()
-      .should('equal', Cypress.env('baseUrl') + '/' + loginCredentials.role);
+      .should(
+        'equal',
+        Cypress.env('baseUrl') + '/' + Credentials.getTeacherCredentials().role
+      );
     cy.get('.pusher > .ui > [href="/teacher/logout"] > .item').click();
     cy.url().should('equal', Cypress.env('baseUrl') + '/');
   });
