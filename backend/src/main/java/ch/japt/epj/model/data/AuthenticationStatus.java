21183f04892568f671bc412c574030a0c6ed5f8e
SBI- <pascal.huerlimann@gmail.com>
2018-05-23 07:33:43 +0200

    Rename headerstatus to reflect actual purpose

--- backend/src/main/java/ch/japt/epj/model/data/AuthenticationStatus.java
+++ backend/src/main/java/ch/japt/epj/model/data/AuthenticationStatus.java
@@ -3,14 +3,13 @@ package ch.japt.epj.model.data;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
-// rename this to authentication status and have routings in here?
-public class HeaderStatus {
+public class AuthenticationStatus {
 
   private final String header;
   private final HttpStatus status;
   private final RoleName role;
 
-  public HeaderStatus(Boolean hunter, RoleName role) {
+  public AuthenticationStatus(Boolean hunter, RoleName role) {
     this.header = hunter ? "X-HUNTeR-Redirect" : "Location";
     this.status = hunter ? HttpStatus.OK : HttpStatus.FOUND;
     this.role = role;
