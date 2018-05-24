ca16326b074e7707c2f434aac429356ee8a80153
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-05-24 09:22:52 +0200

    Add more Modal messages after logout and creation of new participant

--- frontend/src/subSites/Logout.js
+++ frontend/src/subSites/Logout.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import ModalHandler from '../handlers/ModalHandler';
 import pathConfig from '../config/pathConfig';
+import defaultUIConfig from '../config/defaultUIConfig';
 
 export default function Logout() {
   return (
     <div>
       {window.localStorage.removeItem('HUNTeR-Redirect')}
       {window.localStorage.removeItem('HUNTeR-Token')}
-      {window.location.replace(pathConfig.mainURL)}
+      {ModalHandler.getLogoutSuccess()}
+      {setTimeout(() => {
+        window.location.replace(pathConfig.mainURL);
+      }, defaultUIConfig.defaultTimeoutTime)}
     </div>
   );
 }
