e4bb2398bcb35dd0aac54405a23fabf19dc1919e
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-05-25 15:41:39 +0200

    Finalize student score overview

--- frontend/src/subSites/participant/ParticipantScore.js
+++ frontend/src/subSites/participant/ParticipantScore.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {OK} from 'http-status-codes';
 
 import {Card, Dropdown, Grid, Icon, Menu} from 'semantic-ui-react';
-import {isMobile} from 'react-device-detect';
 
 import APIHandler from '../../handlers/APIHandler';
+import viewHandler from '../../handlers/viewHandler';
 
 export default class ParticipantScore extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       trophyColors: ['golden', 'silver', 'bronze', 'red'],
       leaderBoard: [],
       executionId: 1,
@@ -25,13 +26,16 @@ export default class ParticipantScore extends React.Component {
 
   getExecutions = () => {
     APIHandler.getExecutions().then(resData => {
-      const executions = resData.data.content.map(element => {
-        let returnValue = {};
-        returnValue.key = element.id;
-        returnValue.text = element.name;
-        returnValue.value = element.id;
-        return returnValue;
-      });
+      const executions = resData.data.content
+        .map(element => {
+          let returnValue = {};
+          returnValue.key = element.id;
+          returnValue.text = element.id + ' - ' + element.name;
+          returnValue.value = element.id;
+          return returnValue;
+        })
+
+        .sort((a, b) => a.key > b.key);
       this.setState({executions});
     });
   };
@@ -51,56 +55,67 @@ export default class ParticipantScore extends React.Component {
         );
       }
       if (resData.status === OK) {
-        this.setState({leaderBoard});
+        this.setState({leaderBoard, loading: false});
       }
     });
   };
 
   changeExecutionState = (event, data) => {
-    this.setState({executionId: data.value});
+    this.setState({loading: true, executionId: data.value});
     this.getLeaderBoard(data.value);
   };
 
   render() {
     return (
       <Grid padded>
-        <Grid.Row centered>
-          <Card.Group centered>
-            {this.state.leaderBoard.map((element, index) => (
-              <Card
-                key={'scoreCard' + element[1].userName}
-                color={element[1].me ? 'red' : 'green'}
-              >
-                <Card.Content>
-                  <Card.Header>
-                    <Menu text>
-                      <Menu.Item header>{element[1].userName}</Menu.Item>
-                      <Menu.Item>
-                        <Icon
-                          name="trophy"
-                          className={this.state.trophyColors[index] + 'Trophy'}
-                        />
-                      </Menu.Item>
-                      <Menu.Item content={element[1].userScore * 100 + '%'} />
-                      <Menu.Item content={'Rang: ' + element.ranking} />
-                    </Menu>
-                  </Card.Header>
-                  <Card.Description />
-                </Card.Content>
-              </Card>
-            ))}
-          </Card.Group>
-        </Grid.Row>
         <Grid.Row>
           <Dropdown
             fluid
             selection
             closeOnBlur
             scrolling
-            upward={isMobile}
             options={this.state.executions}
             onChange={this.changeExecutionState}
+            defaultValue={1}
           />
+        </Grid.Row>
+        <Grid.Row centered>
+          {this.state.loading ? (
+            viewHandler.getLoadingScreen()
+          ) : (
+            <Card.Group centered>
+              {this.state.leaderBoard.map((element, index) => (
+                <Card
+                  key={'scoreCard' + element[1].userName}
+                  color={element[1].me ? 'red' : 'green'}
+                >
+                  <Card.Content>
+                    <Card.Header>
+                      <Menu text>
+                        <Menu.Item content={'Rang: ' + element.ranking} />
+                        <Menu.Item>
+                          <Icon
+                            name="trophy"
+                            className={
+                              this.state.trophyColors[index] + 'Trophy'
+                            }
+                          />
+                        </Menu.Item>
+                        <Menu.Item
+                          position="right"
+                          content={element[1].userScore * 100 + '%'}
+                        />
+                      </Menu>
+                    </Card.Header>
+                    <Card.Description
+                      className="userScoreName"
+                      content={element[1].userName}
+                    />
+                  </Card.Content>
+                </Card>
+              ))}
+            </Card.Group>
+          )}
         </Grid.Row>
       </Grid>
     );
