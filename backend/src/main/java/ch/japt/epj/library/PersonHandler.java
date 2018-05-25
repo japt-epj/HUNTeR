58135703b2e83792c1587eb59f9c3f82258f97f3
TobiHSR <tobias.saladin@hsr.ch>
2018-05-25 13:12:10 +0200

    Check if first response

---
+++ backend/src/main/java/ch/japt/epj/library/PersonHandler.java
@@ -0,0 +1,13 @@
+package ch.japt.epj.library;
+
+import ch.japt.epj.security.CustomUserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public final class PersonHandler {
+
+  public static Long getCurrentPersonId() {
+    return ((CustomUserDetails)
+            SecurityContextHolder.getContext().getAuthentication().getPrincipal())
+        .getPersonId();
+  }
+}
