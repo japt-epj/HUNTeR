7d2ba90f2479b9803df654e8c45ea717c1a3255d
AndiHSR <andi.hoerler@hsr.ch>
2018-05-27 13:18:14 +0200

    Fix user answer and leaderboard trophy colors

--- frontend/src/subSites/participant/ParticipantExercise.js
+++ frontend/src/subSites/participant/ParticipantExercise.js
@@ -65,12 +65,12 @@ export default class ParticipantExercise extends React.Component {
                         return (
                           <Grid.Row key={element.text}>
                             <Form.Radio
-                              value={index}
+                              value={element.answerId}
                               label={
                                 'Antwort ' + (index + 1) + ' : ' + element.text
                               }
                               onChange={this.handleSelectChange}
-                              checked={this.state.answerId === index}
+                              checked={this.state.answerId === element.answerId}
                             />
                           </Grid.Row>
                         );
