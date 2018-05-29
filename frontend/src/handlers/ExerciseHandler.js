cd838c0164cef83add78026e632fb8b7462ca6c2
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-05-29 11:32:15 +0200

    Add API call on resetPage and restructure function of checkboxes

--- frontend/src/handlers/ExerciseHandler.js
+++ frontend/src/handlers/ExerciseHandler.js
@@ -11,64 +11,82 @@ import ShowExerciseEditModal from '../components/ShowExerciseEditModal';
 import PaginationHandler from './PaginationHandler';
 
 export default {
-  handleSelection(event, checkbox) {
+  handleSingleSelection(event, checkbox) {
     let selectedCheckboxes = [...this.state.selectedCheckboxes];
     let bulkCheckboxes = [...this.state.bulkCheckboxes];
-    let pageNumber = this.state.pageNumber;
-    let pageNumberSelectedExercises = this.state.pageNumberSelectedExercises;
     let selectedPositions = new Map(this.state.selectedPositions);
-    const startNameBulkCheckbox = 'BulkCheckbox';
+    const currentBulkCheckboxId = 'BulkCheckbox' + this.state.pageNumber;
     if (checkbox.checked) {
-      if (checkbox.name.startsWith(startNameBulkCheckbox)) {
-        bulkCheckboxes.push(checkbox.id);
-        this.state.exercises.forEach(element => {
-          if (selectedCheckboxes.indexOf(element.id) === -1) {
-            selectedCheckboxes.push(element.id);
-            selectedPositions.set(element.id, undefined);
-          }
-        });
-      } else {
-        selectedPositions.set(checkbox.id, undefined);
-        selectedCheckboxes.push(checkbox.id);
-      }
+      selectedPositions.set(checkbox.id, undefined);
+      selectedCheckboxes.push(checkbox.id);
     } else {
-      if (checkbox.name.startsWith(startNameBulkCheckbox)) {
-        bulkCheckboxes.splice(selectedCheckboxes.lastIndexOf(checkbox.id), 1);
-        this.state.exercises.forEach(element => {
-          if (selectedCheckboxes.indexOf(element.id) !== -1) {
-            selectedCheckboxes.splice(
-              selectedCheckboxes.indexOf(element.id),
-              1
-            );
-            selectedPositions.delete(element.id);
-          }
-        });
-      } else {
-        bulkCheckboxes.splice(
-          selectedCheckboxes.lastIndexOf(startNameBulkCheckbox + pageNumber),
-          1
-        );
-        selectedPositions.delete(checkbox.id);
-        selectedCheckboxes.splice(
-          selectedCheckboxes.lastIndexOf(checkbox.id),
-          1
-        );
-      }
+      selectedPositions.delete(checkbox.id);
+      selectedCheckboxes.splice(selectedCheckboxes.lastIndexOf(checkbox.id), 1);
+      bulkCheckboxes.splice(
+        selectedCheckboxes.lastIndexOf(currentBulkCheckboxId),
+        1
+      );
     }
-    this.setState({
+
+    this.updateSelection({
       bulkCheckboxes,
-      selectedCheckboxes: selectedCheckboxes.sort((a, b) => a > b),
+      selectedCheckboxes,
       selectedPositions
     });
+  },
+
+  handleBulkSelection(event, checkbox) {
+    let selectedCheckboxes = [...this.state.selectedCheckboxes];
+    let bulkCheckboxes = [...this.state.bulkCheckboxes];
+    let selectedPositions = new Map(this.state.selectedPositions);
+    if (checkbox.checked) {
+      this.state.exercises.forEach(element => {
+        if (selectedCheckboxes.indexOf(element.id) === -1) {
+          selectedCheckboxes.push(element.id);
+          console.log(selectedCheckboxes);
+          selectedPositions.set(element.id, undefined);
+        }
+      });
+      bulkCheckboxes.push(checkbox.id);
+    } else {
+      this.state.exercises.forEach(element => {
+        if (selectedCheckboxes.indexOf(element.id) !== -1) {
+          selectedCheckboxes.splice(selectedCheckboxes.indexOf(element.id), 1);
+          selectedPositions.delete(element.id);
+        }
+      });
+      bulkCheckboxes.splice(selectedCheckboxes.lastIndexOf(checkbox.id), 1);
+    }
+    this.updateSelection({
+      bulkCheckboxes,
+      selectedCheckboxes,
+      selectedPositions
+    });
+  },
+
+  updateSelection(values) {
+    this.setState({
+      bulkCheckboxes: values.bulkCheckboxes,
+      selectedCheckboxes: values.selectedCheckboxes.sort((a, b) => a > b),
+      selectedPositions: values.selectedPositions
+    });
+    console.log(
+      (this.state.pageNumberSelectedExercises - 1) * this.exerciseLimitPerPage
+    );
+    console.log(
+      this.state.pageNumberSelectedExercises * this.exerciseLimitPerPage
+    );
     APIHandler.getExerciseArray(
-      selectedCheckboxes.slice(
-        (pageNumberSelectedExercises - 1) * this.exerciseLimitPerPage,
-        pageNumberSelectedExercises * this.exerciseLimitPerPage
+      values.selectedCheckboxes.slice(
+        (this.state.pageNumberSelectedExercises - 1) *
+          this.exerciseLimitPerPage,
+        this.state.pageNumberSelectedExercises * this.exerciseLimitPerPage
       )
     ).then(resData => {
       if (resData.status === OK) {
         this.setState({
-          selectedExercises: selectedCheckboxes.length !== 0 ? resData.data : []
+          selectedExercises:
+            values.selectedCheckboxes.length !== 0 ? resData.data : []
         });
       } else {
         console.error('Error:' + resData);
@@ -150,7 +168,7 @@ export default {
               TableHandler.getBulkCheckbox(
                 this.state.pageNumber,
                 this.state.bulkCheckboxes,
-                this.handleSelection
+                this.handleBulkSelection
               )}
             {TableHandler.getTableHeader(headerElements)}
           </Table.Row>
@@ -164,7 +182,7 @@ export default {
                     <Checkbox
                       id={element.id}
                       name={element.name}
-                      onChange={this.handleSelection}
+                      onChange={this.handleSingleSelection}
                       checked={
                         this.state.selectedCheckboxes.indexOf(element.id) !== -1
                       }
