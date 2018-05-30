af3991a0fa90a9284cb2447a0cf07083c13767dd
SBI- <pascal.huerlimann@gmail.com>
2018-05-30 17:10:49 +0200

    Make parent test abstract

--- backend/src/test/java/ch/japt/epj/controller/AuthenticatedControllerTest.java
+++ backend/src/test/java/ch/japt/epj/controller/AuthenticatedControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class AuthenticatedControllerTest {
+public abstract class AuthenticatedControllerTest {
 
   protected String completeToken;
   @Autowired private JwtTokenProvider tokenProvider;
