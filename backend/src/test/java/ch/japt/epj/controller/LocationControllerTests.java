f09f94bfd916f7fff1b2a31d92f022257d7e4a63
SBI- <pascal.huerlimann@gmail.com>
2018-05-30 21:06:12 +0200

    Add test class for location controller

---
+++ backend/src/test/java/ch/japt/epj/controller/LocationControllerTests.java
@@ -0,0 +1,18 @@
+package ch.japt.epj.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class LocationControllerTests extends AuthenticatedControllerTest {
+
+  @Test
+  public void getNextLocationInExecution() {}
+}
