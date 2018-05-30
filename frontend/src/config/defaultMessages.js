54b24d0b2a8929ea671b9780e41fa3a2e79d588c
AndiHSR <andi.hoerler@hsr.ch>
2018-05-30 21:41:56 +0200

    Remove unneeded consoleLog

--- frontend/src/config/defaultMessages.js
+++ frontend/src/config/defaultMessages.js
@@ -3,7 +3,6 @@ import {isMobile} from 'react-device-detect';
 let defaultMessages = {
   hideAgreement: () => {
     let hideAgreement = window.sessionStorage.getItem('HUNTeR-hideAgreement');
-    console.log(Boolean(JSON.parse(hideAgreement)));
     return Boolean(JSON.parse(hideAgreement));
   },
 
