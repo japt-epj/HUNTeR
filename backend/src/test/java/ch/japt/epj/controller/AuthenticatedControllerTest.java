3cb2e34f0a3ab4bd8680743638bc37bbcee7e8f7
SBI- <pascal.huerlimann@gmail.com>
2018-05-30 22:35:28 +0200

    Extract test user authentication
    This is useful for being able to change the current user to a different
    user for certain tests, such as being able to authenticate as a student
    for finding the next location of an exercise.

--- backend/src/test/java/ch/japt/epj/controller/AuthenticatedControllerTest.java
+++ backend/src/test/java/ch/japt/epj/controller/AuthenticatedControllerTest.java
@@ -1,27 +1,18 @@
 package ch.japt.epj.controller;
 
-import ch.japt.epj.security.JwtTokenProvider;
+import ch.japt.epj.helper.TestUserAuthenticator;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 public abstract class AuthenticatedControllerTest {
 
   protected String completeToken;
-  @Autowired private JwtTokenProvider tokenProvider;
-  @Autowired private AuthenticationManager authenticationManager;
+  @Autowired TestUserAuthenticator authenticator;
 
   @Before
   public void getToken() {
     String validEmail = "tobias.saladin@hsr.ch";
     String validPassword = "tobias";
-    Authentication authentication =
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(validEmail, validPassword));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    completeToken = "Bearer " + tokenProvider.generateToken(authentication);
+    completeToken = authenticator.authenticate(validEmail, validPassword);
   }
 }
