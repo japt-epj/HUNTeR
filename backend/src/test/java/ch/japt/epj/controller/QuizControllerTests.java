1409951c21880cd5141c677d39f693ded18ba2de
SBI- <pascal.huerlimann@gmail.com>
2018-05-30 21:05:42 +0200

    Add quiz controller tests

---
+++ backend/src/test/java/ch/japt/epj/controller/QuizControllerTests.java
@@ -0,0 +1,13 @@
+package ch.japt.epj.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class QuizControllerTests extends AuthenticatedControllerTest {}
