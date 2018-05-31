27f040bd841f8cce1806985bb34d0342566ccda5
SBI- <pascal.huerlimann@gmail.com>
2018-05-31 09:20:53 +0200

    Add response controller tests

---
+++ backend/src/test/java/ch/japt/epj/controller/ResponseControllerTests.java
@@ -0,0 +1,17 @@
+package ch.japt.epj.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class ResponseControllerTests extends AuthenticatedControllerTest {
+  @Test
+  public void createNewResponse() throws Exception {}
+}
