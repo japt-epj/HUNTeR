7f967bf02a023151bf98b7e86071cff55df56ac7
SBI- <pascal.huerlimann@gmail.com>
2018-05-31 08:17:44 +0200

    Add tests for getting single users

--- backend/src/test/java/ch/japt/epj/controller/PersonControllerTests.java
+++ backend/src/test/java/ch/japt/epj/controller/PersonControllerTests.java
@@ -1,7 +1,9 @@
 package ch.japt.epj.controller;
 
+import static ch.japt.epj.helper.PaginationChecker.isPaginated;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,17 +28,54 @@ public class PersonControllerTests extends AuthenticatedControllerTest {
   @Test
   public void getExistingPersonById() throws Exception {
     MockHttpServletRequestBuilder request =
-        MockMvcRequestBuilders.get("/api/person/1")
+        MockMvcRequestBuilders.get("/api/person/5")
+            .header("Authorization", token)
+            .contentType(MediaType.APPLICATION_JSON);
+
+    ResultActions actions =
+        mvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    assertPersonPayload(actions, "$[0]");
+  }
+
+  @Test
+  public void getCurrentPerson() throws Exception {
+    setCurrentToken("dolores.abernathy@host.westworld.com", "dolores");
+
+    MockHttpServletRequestBuilder request =
+        MockMvcRequestBuilders.get("/api/person/current")
+            .header("Authorization", token)
+            .contentType(MediaType.APPLICATION_JSON);
+
+    ResultActions actions =
+        mvc.perform(request)
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    assertPersonPayload(actions, "$");
+  }
+
+  @Test
+  public void checkPersonPagination() throws Exception {
+    MockHttpServletRequestBuilder request =
+        MockMvcRequestBuilders.get("/api/person")
             .header("Authorization", token)
             .contentType(MediaType.APPLICATION_JSON);
 
     mvc.perform(request)
         .andExpect(status().isOk())
         .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$[0]").exists())
-        .andExpect(jsonPath("$[0].firstName").value("Tobias"))
-        .andExpect(jsonPath("$[0].lastName").value("Saladin"))
-        .andExpect(jsonPath("$[0].email").value("tobias.saladin@hsr.ch"))
-        .andExpect(jsonPath("$[0].password").doesNotExist());
+        .andExpect(isPaginated());
+  }
+
+  private static void assertPersonPayload(ResultActions mvc, String object) throws Exception {
+    mvc.andExpect(jsonPath(object + ".id").value(5))
+        .andExpect(jsonPath(object + ".firstName").value("Dolores"))
+        .andExpect(jsonPath(object + ".lastName").value("Abernathy"))
+        .andExpect(jsonPath(object + ".email").value("dolores.abernathy@host.westworld.com"))
+        .andExpect(jsonPath(object + ".password").value(IsNull.nullValue()))
+        .andExpect(jsonPath(object + ".schools").isArray());
   }
 }
