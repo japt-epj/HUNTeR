3516a4fc3eded7deb8f780ffc19c12aad105ab00
SBI- <pascal.huerlimann@gmail.com>
2018-05-31 10:33:41 +0200

    Add basic score test

--- backend/src/test/java/ch/japt/epj/controller/ScoreControllerTests.java
+++ backend/src/test/java/ch/japt/epj/controller/ScoreControllerTests.java
@@ -1,21 +1,33 @@
 package ch.japt.epj.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @AutoConfigureMockMvc
-public class ScoreControllerTests {
+public class ScoreControllerTests extends AuthenticatedControllerTest {
   @Autowired private MockMvc mvc;
 
   @Test
-  public void getScoreForExecution() throws Exception {}
+  public void getScoreForExecution() throws Exception {
+    MockHttpServletRequestBuilder request =
+        MockMvcRequestBuilders.get("/api/score/1")
+            .header("Authorization", token)
+            .accept(MediaType.APPLICATION_JSON);
+
+    mvc.perform(request).andExpect(status().isOk());
+  }
 }
