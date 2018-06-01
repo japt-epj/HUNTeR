1804bcbf3655e4c3cb6523ef1d35ca347f1e92d3
SBI- <pascal.huerlimann@gmail.com>
2018-06-01 09:24:47 +0200

    Refactor response model

--- backend/src/main/java/ch/japt/epj/repository/ResponseRepository.java
+++ backend/src/main/java/ch/japt/epj/repository/ResponseRepository.java
@@ -19,4 +19,12 @@ public interface ResponseRepository extends CrudRepository<Response, Long> {
           + "where r.exercise.exerciseId = ?2 "
           + "and r.person.personId = ?1")
   Optional<Response> answerExists(Long person, Long exercise);
+
+  @Query(
+      "select new Response(p, e, a) "
+          + "from Person p "
+          + "inner join Exercise e on e.exerciseId = ?2 "
+          + "inner join Answer a on a.answerId = ?3 "
+          + "where p.personId = ?1")
+  Response fromDtoData(Long personId, Long exerciseId, Long answerId);
 }
