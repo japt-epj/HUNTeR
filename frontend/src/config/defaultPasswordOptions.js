2bb1995ffd539bc11ba08c248a3f30f75aa03297
rubber-croc <23471915+rubber-croc@users.noreply.github.com>
2018-06-16 03:06:14 +0200

    Extract Strings to defaultPasswordOptions

--- frontend/src/config/defaultPasswordOptions.js
+++ frontend/src/config/defaultPasswordOptions.js
@@ -1,12 +1,21 @@
 let defaultPasswordOptions = {
   minPasswordLength: 10,
+  listHeader: 'Dem Passwort fehlt: ',
   passwordComponents: new Map([
     ['min', 'die Minimallänge (min. 10 Zeichen)'],
     ['uppercase', 'ein Grossbuchstabe'],
     ['lowercase', 'ein Kleinbuchstabe'],
     ['digits', 'eine Nummer'],
     ['symbols', 'ein Sonderzeichen']
-  ])
+  ]),
+  currentPasswordError: {
+    header: 'Altes Passwort falsch',
+    content: 'Das alte Passwort war falsch.'
+  },
+  newPasswordError: {
+    header: 'Altes Passwort falsch',
+    content: 'Die Passwörter sind nicht identisch'
+  }
 };
 
 export default Object.freeze(Object.assign({}, defaultPasswordOptions));
